f600451997a95a5d6997fb5914b9c112c051763c
heesunee <heesun729@uos.ac.kr>
2025-06-30 03:07:15 +0900

    init: lib 파일 이동 (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import queryClient from './query-client';
+import queryClient from './shared/libs/query-client';
 
 const App = () => {
   return (
