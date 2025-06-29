85eb70d0b4e3b73089b9be7d90191d0fdb724166
bongtta <kce0419@naver.com>
2025-06-29 15:21:59 +0900

    chore: 테스트 코드 삭제 (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 const App = () => {
-  return (
-    <div className="p-8 space-y-6 bg-[var(--main-1000)] text-[var(--gray-white)]">
-      <h1 className="title_24_sb">타이틀 24 SB</h1>
-      <p className="cap_14_m">캡션 텍스트</p>
-      <div className="w-40 h-20 shadow-1 bg-[var(--sub-900)]" />
-    </div>
-  );
+  return <div>메잇볼</div>;
 };
 
 export default App;
