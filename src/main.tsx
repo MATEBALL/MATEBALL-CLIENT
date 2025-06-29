202725a843dac3eeb1714bda040b9efa7feccf6c
bongtta <kce0419@naver.com>
2025-06-29 15:23:56 +0900

    chore: import 위치 변경 (#11)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-import "./shared/styles/reset.css";
 import "./shared/styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
