d3b534bfb7c360d84c09fe091b5185ac1828f452
bongtta <kce0419@naver.com>
2025-07-03 05:12:41 +0900

    feat: 월간 달력 구현 (#25)

--- src/shared/components/calendar/MonthCalendar.tsx
+++ src/shared/components/calendar/MonthCalendar.tsx
@@ -1,18 +1,36 @@
+import { format } from 'date-fns';
+import { getMonthGrid } from '../../utils/calendar';
+import { useState } from 'react';
+
 const MonthCalendar = () => {
+  const [value, setValue] = useState(new Date());
+  const days = getMonthGrid(value);
+
   return (
-    <>
-        <div>
-            <div></div>
-            <p></p>
-            <div></div>
-        </div>
+    <div  className='flex flex-col gap-[1.2rem]'>
+      <div>
+        <div></div>
+        <p className="head_20_sb text-gray-black text-center">{format(value, 'yyyy.MM')}</p>
+        <div></div>
+      </div>
 
-        <div>
-            <div></div>
-            <div></div>
+      <div>
+        <div className='grid grid-cols-7 text-center cap_14_sb'>
+            {['일', '월', '화', '수', '목', '금', '토'].map((d) => <div className='px-[1.75rem] py-[1.35rem]' key={d}>{d}</div>)}
         </div>
-    </>
-  )
-}
+        <div className="grid grid-cols-7 cap_14_m text-gray-900 gap-y-[0.4rem]">
+          {days.map((day) => (
+            <button 
+              onClick={() => setValue(day)}
+              className='px-[1.65rem] py-[1.35rem]'
+            >
+              {format(day, 'd')}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MonthCalendar
+export default MonthCalendar;
