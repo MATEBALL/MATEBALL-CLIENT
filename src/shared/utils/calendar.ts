120d37883f4285e5d8557d68eccaab7d4f123b7c
bongtta <kce0419@naver.com>
2025-07-04 01:32:51 +0900

    feat: 주간 캘린더 로직 구현 (#25)

--- src/shared/utils/calendar.ts
+++ src/shared/utils/calendar.ts
@@ -1,5 +1,11 @@
 import { addDays, endOfMonth, isAfter, startOfMonth, startOfWeek } from 'date-fns';
 
+export const getWeekDays = (baseDate: Date): Date[] => {
+  const start = startOfWeek(baseDate, { weekStartsOn: 0 });
+
+  return Array.from({ length: 7 }, (_, i) => addDays(start, i));
+};
+
 export const getMonthGrid = (date: Date): Date[] => {
   const start = startOfWeek(startOfMonth(date), { weekStartsOn: 0 });
   const end = addDays(endOfMonth(date), 6 - endOfMonth(date).getDay());
