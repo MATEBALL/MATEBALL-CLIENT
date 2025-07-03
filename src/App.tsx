e6e5f3741eb44f989c6e97b7973f6da6f11b4ed9
heesunee <heesun729@uos.ac.kr>
2025-07-03 18:54:36 +0900

    fix: biome check (#32)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { RouterProvider } from 'react-router-dom';
+import queryClient from '@libs/query-client';
 import { router } from '@routes/Router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import queryClient from '@libs/query-client';
-
-
+import { RouterProvider } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 };
 
 export default App;
