f34f3526fbb6325c6fef7696e9db976d99e2e3df
yeeeww <yeangsshi@ewhain.net>
2025-07-03 13:57:09 +0900

    chore: 확인용 코드 삭제(#10)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { router } from '@routes/Router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import queryClient from '@libs/query-client';
-import Icon from "./shared/components/Icon";
 
 
 
@@ -13,12 +12,6 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
-      <div>
-			메잇볼
-      <Icon name="ic-home-filled" width={40} height={40} className="text-main-700" />
-      <Icon name="ic-home-filled" size={20} className="text-main-700" />
-      <Icon name="ic-home-filled" rotate={90} className="text-gray-500" />
-		</div>
     </QueryClientProvider>
   );
 
