befe209875fbb5a6f809902884868f7871845821
heesunee <heesun729@uos.ac.kr>
2025-07-05 06:13:12 +0900

    feat: dialog 컴포넌트 완성 (#51)

--- src/shared/components/dialog/dialog.tsx
+++ src/shared/components/dialog/dialog.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode } from 'react';
 
 interface DialogProps {
-  ment?: string;
+  info?: ReactNode;
   children?: ReactNode;
 }
-const Dialog = ({ ment, children }: DialogProps) => {
+const Dialog = ({ info, children }: DialogProps) => {
   return (
-    <div>
-      <p>{ment}</p>
+    <div className="w-[34.3rem] flex-col-center rounded-[1.6rem] bg-gray-white px-[3.2rem] pt-[4rem] pb-[3.2rem] shadow-1">
+      <p className="subhead_18_sb text-center">{info}</p>
       {children}
     </div>
   );
