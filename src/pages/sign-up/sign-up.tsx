08b54df3c4dbdbca20df1338c2601100c8be8b83
heesunee <heesun729@uos.ac.kr>
2025-07-05 05:47:41 +0900

    feat: x svg 고정으로 수정 (#38)

--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -1,15 +1,7 @@
-import { NICKNAME_PLACEHOLDER } from '@components/input/constants/validation';
-import Input from '@components/input/input';
-
 const SignUp = () => {
   return (
     <div>
       회원가입페이지
-<<<<<<< HEAD
-      <Input placeholder={NICKNAME_PLACEHOLDER} />
-=======
-      <Input placeholder={NICKNAME_PLACEHOLDER} svg="x" />
->>>>>>> 6c0fb11 (feat: input 컴포넌트 작성 (#38))
     </div>
   );
 };
--- src/shared/components/input/input.tsx
+++ src/shared/components/input/input.tsx
@@ -3,6 +3,7 @@ import { type InputHTMLAttributes, useState } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 <<<<<<< HEAD
+<<<<<<< HEAD
   placeholder?: string;
 }
 
@@ -19,18 +20,33 @@ const Input = ({ placeholder, ...props }: InputProps) => {
   };
 =======
   svg?: string;
+=======
+>>>>>>> 7d251b9 (feat: x svg 고정으로 수정 (#38))
   placeholder?: string;
 }
 
-const Input = ({ svg, placeholder, ...props }: InputProps) => {
+const Input = ({ placeholder, ...props }: InputProps) => {
   const [isFocused, setIsFocused] = useState(false);
+<<<<<<< HEAD
 >>>>>>> 6c0fb11 (feat: input 컴포넌트 작성 (#38))
+=======
+  const [value, setValue] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleClear = () => {
+    setValue('');
+  };
+>>>>>>> 7d251b9 (feat: x svg 고정으로 수정 (#38))
 
   return (
     <div className="body_16_m h-[5.6rem] w-[34.3rem] flex-row-between rounded-[12px] bg-gray-100 p-[1.6rem] ">
       <input
         type="text"
         className="flex-1 text-gray-black placeholder:text-gray-500"
+<<<<<<< HEAD
 <<<<<<< HEAD
         value={value}
         onFocus={() => setIsFocused(true)}
@@ -41,13 +57,21 @@ const Input = ({ svg, placeholder, ...props }: InputProps) => {
       />
       {(isFocused || value) && <Icon name="ic-x" width={2.4} height={2.4} onClick={handleClear} />}
 =======
+=======
+        value={value}
+>>>>>>> 7d251b9 (feat: x svg 고정으로 수정 (#38))
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         placeholder={isFocused ? '' : placeholder}
+        onChange={handleChange}
         {...props}
       />
+<<<<<<< HEAD
       {isFocused && <Icon name={`ic-${svg}`} width={2.4} height={2.4} />}
 >>>>>>> 6c0fb11 (feat: input 컴포넌트 작성 (#38))
+=======
+      {(isFocused || value) && <Icon name="ic-x" width={2.4} height={2.4} onClick={handleClear} />}
+>>>>>>> 7d251b9 (feat: x svg 고정으로 수정 (#38))
     </div>
   );
 };
