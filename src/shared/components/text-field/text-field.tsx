6e46d848f69ab4ff13c13a142d9e3aa2f1a85f9d
heesunee <heesun729@uos.ac.kr>
2025-07-05 04:48:05 +0900

    style: input style class 작성 (#38)

--- src/shared/components/text-field/text-field.tsx
+++ src/shared/components/text-field/text-field.tsx
@@ -1,5 +1,12 @@
 const TextField = () => {
-  return <input />;
+  return (
+    <div>
+      <input
+        type="text"
+        className="body_16_m height-[5.6rem] w-[34.3rem] flex-row rounded-[12px] bg-gray-100 p-[1.6rem] text-gray-500"
+      />
+    </div>
+  );
 };
 
 export default TextField;
