80c8934b53ed513953070c8d9f2cf9eceed0ded2
heesunee <heesun729@uos.ac.kr>
2025-07-05 05:47:41 +0900

    feat: x svg 고정으로 수정 (#38)

--- src/shared/components/input/input.tsx
+++ src/shared/components/input/input.tsx
@@ -2,24 +2,34 @@ import Icon from '@components/Icon';
 import { type InputHTMLAttributes, useState } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  svg?: string;
   placeholder?: string;
 }
 
-const Input = ({ svg, placeholder, ...props }: InputProps) => {
+const Input = ({ placeholder, ...props }: InputProps) => {
   const [isFocused, setIsFocused] = useState(false);
+  const [value, setValue] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleClear = () => {
+    setValue('');
+  };
 
   return (
     <div className="body_16_m h-[5.6rem] w-[34.3rem] flex-row-between rounded-[12px] bg-gray-100 p-[1.6rem] ">
       <input
         type="text"
         className="flex-1 text-gray-black placeholder:text-gray-500"
+        value={value}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         placeholder={isFocused ? '' : placeholder}
+        onChange={handleChange}
         {...props}
       />
-      {isFocused && <Icon name={`ic-${svg}`} width={2.4} height={2.4} />}
+      {(isFocused || value) && <Icon name="ic-x" width={2.4} height={2.4} onClick={handleClear} />}
     </div>
   );
 };
