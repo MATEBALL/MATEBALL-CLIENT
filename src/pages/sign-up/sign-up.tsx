e5d496f2088fd72d449bb01406abb6dab7ba09a9
heesunee <heesun729@uos.ac.kr>
2025-07-05 05:38:43 +0900

    feat: input 컴포넌트 작성 (#38)

--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -5,7 +5,11 @@ const SignUp = () => {
   return (
     <div>
       회원가입페이지
+<<<<<<< HEAD
       <Input placeholder={NICKNAME_PLACEHOLDER} />
+=======
+      <Input placeholder={NICKNAME_PLACEHOLDER} svg="x" />
+>>>>>>> 6c0fb11 (feat: input 컴포넌트 작성 (#38))
     </div>
   );
 };
--- src/shared/components/input/input.tsx
+++ src/shared/components/input/input.tsx
@@ -2,6 +2,7 @@ import Icon from '@components/Icon';
 import { type InputHTMLAttributes, useState } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+<<<<<<< HEAD
   placeholder?: string;
 }
 
@@ -16,12 +17,21 @@ const Input = ({ placeholder, ...props }: InputProps) => {
   const handleClear = () => {
     setValue('');
   };
+=======
+  svg?: string;
+  placeholder?: string;
+}
+
+const Input = ({ svg, placeholder, ...props }: InputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+>>>>>>> 6c0fb11 (feat: input 컴포넌트 작성 (#38))
 
   return (
     <div className="body_16_m h-[5.6rem] w-[34.3rem] flex-row-between rounded-[12px] bg-gray-100 p-[1.6rem] ">
       <input
         type="text"
         className="flex-1 text-gray-black placeholder:text-gray-500"
+<<<<<<< HEAD
         value={value}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
@@ -30,6 +40,14 @@ const Input = ({ placeholder, ...props }: InputProps) => {
         {...props}
       />
       {(isFocused || value) && <Icon name="ic-x" width={2.4} height={2.4} onClick={handleClear} />}
+=======
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        placeholder={isFocused ? '' : placeholder}
+        {...props}
+      />
+      {isFocused && <Icon name={`ic-${svg}`} width={2.4} height={2.4} />}
+>>>>>>> 6c0fb11 (feat: input 컴포넌트 작성 (#38))
     </div>
   );
 };
