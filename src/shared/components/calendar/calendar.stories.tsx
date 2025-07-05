075aa767992c0be78ab620dbf9f5a127f1a6ab28
bongtta <kce0419@naver.com>
2025-07-05 21:31:25 +0900

    feat: 스토리북 작성 (#25)

---
+++ src/shared/components/calendar/calendar.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import MonthCalendar from './month-calendar';
+import WeekCalendar from './week-calendar';
+
+const meta: Meta = {
+  title: 'Components/Calendar',
+  component: MonthCalendar,
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+type Story = StoryObj;
+
+export const Month: Story = {
+  render: () => <MonthCalendar />,
+};
+
+export const Week: Story = {
+  render: () => {
+    const [value, setValue] = useState(new Date());
+    return <WeekCalendar value={value} onChange={setValue} />;
+  },
+};
