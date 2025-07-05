697c0a1225442b25bf34c63383c1d547de51df5d
Dubabbi <thdms3160@naver.com>
2025-07-05 14:39:18 +0900

    style: bar tab 스타일 수정 (#56)

--- src/shared/components/tab/bar-tab/bar-tab-item.tsx
+++ src/shared/components/tab/bar-tab/bar-tab-item.tsx
@@ -1,8 +1,12 @@
 import { cn } from '@libs/cn';
 import type { FC } from 'react';
-
-// 홈탭 vs 그룹탭 기준
-type TabColorMode = 'home' | 'group';
+import type { TabColorMode } from '@styles/bar-tab.styles';
+import {
+  getTextColorClass,
+  getBorderColorClass,
+  getHeightClass,
+  getTypographyClass,
+} from '@styles/bar-tab.styles';
 
 interface BarTabItemProps {
   label: string;
@@ -12,38 +16,23 @@ interface BarTabItemProps {
 }
 
 const BarTabItem: FC<BarTabItemProps> = ({ label, isActive, colorMode, onClick }) => {
-  const textColorClass = (() => {
-    if (isActive) {
-      return colorMode === 'home' ? 'text-gray-white' : 'text-gray-black';
-    } else {
-      return colorMode === 'home' ? 'text-gray-500' : 'text-gray-600';
-    }
-  })();
-
-  const borderColorClass = (() => {
-    if (isActive) {
-      return colorMode === 'home' ? 'border-gray-white' : 'border-gray-black';
-    } else {
-      return 'border-transparent';
-    }
-  })();
-
-
   return (
     <button
       type="button"
       data-active={isActive}
       onClick={onClick}
       className={cn(
-        'w-12 border-b-2',
-        borderColorClass,
-        'inline-flex flex-col justify-start items-center gap-1 cursor-pointer',
+        'w-[4.8rem] border-b-[0.2rem]',
+        getBorderColorClass(isActive, colorMode),
+        getHeightClass(colorMode),
+        'inline-flex flex-col justify-start items-center cursor-pointer px-[0.6rem] whitespace-nowrap',
       )}
     >
       <span
         className={cn(
-          'text-xl font-semibold leading-loose font-[Pretendard] text-center justify-start',
-          textColorClass,
+          getTypographyClass(colorMode),
+          'text-center',
+          getTextColorClass(isActive, colorMode),
         )}
       >
         {label}
