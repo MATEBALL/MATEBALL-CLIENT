9852f0bb54b5067d65b538b974cd1ab24d3a84fa
heesunee <heesun729@uos.ac.kr>
2025-07-07 21:46:01 +0900

    feat: dialog stories 설명 추가 (#51)

--- src/shared/components/dialog/dialog.stories.tsx
+++ src/shared/components/dialog/dialog.stories.tsx
@@ -4,7 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 const meta: Meta<typeof Dialog> = {
   title: 'common/Dialog',
   component: Dialog,
-    parameters: {
+  parameters: {
     docs: {
       description: {
         component: `
