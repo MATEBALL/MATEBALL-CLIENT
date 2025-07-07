274f04f2bdd77a5e9620acee27ebcc53dd43742c
heesunee <heesun729@uos.ac.kr>
2025-07-08 03:56:49 +0900

    fix: 테스트 코드 삭제 (#72)

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,5 @@
-import CardBanner from '@components/card/card-banner/card-banner';
-
 const Home = () => {
-  return (
-    <div>
-      홈<CardBanner subText="경기일 선택하고" text="나랑 딱 맞는 직관 메이트 찾자!" />
-    </div>
-  );
+  return <div>홈</div>;
 };
 
 export default Home;
