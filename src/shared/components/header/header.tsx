bc659baf739c0420136ba09e0ef2c28c3e273e26
heesunee <heesun729@uos.ac.kr>
2025-07-07 23:47:56 +0900

    feat: header 컴포넌트 구현 (#65)

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,5 +1,18 @@
+import { getHeaderContent } from '@components/header/utils/get-header';
+import { useLocation } from 'react-router-dom';
+
 const Header = () => {
-  return <header>헤더</header>;
+  const location = useLocation();
+  const urlParams = new URLSearchParams(location.search);
+  const isFail = urlParams.get('fail') === 'true';
+
+  return (
+    <div className="bg-gray-black">
+      <header className="h-[5.6rem] py-[1.55rem] pl-[2rem]">
+        {getHeaderContent(location.pathname, isFail)}
+      </header>
+    </div>
+  );
 };
 
 export default Header;
