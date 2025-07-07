7017c429cdd1c30b687f3734abfa785a81477bdc
Dubabbi <thdms3160@naver.com>
2025-07-07 19:32:55 +0900

    feat: chip 관련 작업 전체 정리

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import path, { resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { resolve } from 'node:path';
 import svgSprite from '@pivanov/vite-plugin-svg-sprite';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
@@ -14,7 +14,7 @@ export default defineConfig({
   plugins: [
     react(),
     svgSprite({
-      iconDirs: [resolve(dirname, 'src/shared/assets/svgs')],
+      iconDirs: [resolve(__dirname, 'src/shared/assets/svgs')],
       symbolId: 'icon-[name]',
       inject: 'body-last',
     }),
