38a58de9b3df2ede6a313e72d563f1f19329148e
Dubabbi <thdms3160@naver.com>
2025-07-07 19:32:55 +0900

    feat: chip 관련 작업 전체 정리

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { router } from '@routes/router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
+import ChipList from '@components/chip/chip-list';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <div style={{ padding: '2rem' }}>
+        <ChipList names={['한화', '직관먹방러']} />
+      </div>
+
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
