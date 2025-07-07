3eb1d82bfff2f80852013c0b4f0cea06db2fd2c2
bongtta <kce0419@naver.com>
2025-07-07 23:05:18 +0900

    feat: bottom-navigation 아이콘 수정 (#53)

--- src/shared/components/bottom-navigation/bottom-navigation.tsx
+++ src/shared/components/bottom-navigation/bottom-navigation.tsx
@@ -8,7 +8,7 @@ const BottomNavigation = () => {
         <p className="cap_12_m text-gray-black">홈</p>
       </div>
       <div className="h-[4.8rem] w-[4.8rem] flex-col-center gap-[0.4rem]">
-        <Icon name="ic-matchinglist" width={2.4} height={2.4} />
+        <Icon name="ic-matchinglist-lined" width={2.4} height={2.4} />
         <p className="cap_12_m text-gray-black">매칭 현황</p>
       </div>
       <div className="h-[4.8rem] w-[4.8rem] flex-col-center gap-[0.4rem]">
