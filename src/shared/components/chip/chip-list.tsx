cc680c4114adac8ed696846b28c091278d8fa6b6
Dubabbi <thdms3160@naver.com>
2025-07-07 20:42:36 +0900

    refactor: chip 폴더 위치 수정

--- src/shared/components/chip/chip-list.tsx
+++ src/shared/components/chip/chip-list.tsx
@@ -1,5 +1,5 @@
 import Chip from '@components/chip/chip';
-import { chipVariants } from '@styles/chip-variants';
+import { chipVariants } from '@components/chip/styles/chip-variants';
 import type { VariantProps } from 'class-variance-authority';
 
 type ChipColor = NonNullable<VariantProps<typeof chipVariants>['bgColor']>;
--- src/shared/components/chip/styles/chip-variants.ts
+++ src/shared/components/chip/styles/chip-variants.ts
@@ -1,4 +1,3 @@
-// chip-variants.ts
 import { cva } from 'class-variance-authority';
 
 export const chipVariantOptions = {
