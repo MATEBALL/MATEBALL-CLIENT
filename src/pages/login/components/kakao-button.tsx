b87aa2b9f96617648fe226cad0dca8d37abc6783
heesunee <heesun729@uos.ac.kr>
2025-07-08 02:39:07 +0900

    fix: 변수명 수정 (#67)

--- src/pages/login/components/kakao-button.tsx
+++ src/pages/login/components/kakao-button.tsx
@@ -1,13 +1,16 @@
 import Icon from '@components/icon/icon';
 
 const KakaoButton = () => {
-  const REST_API = import.meta.env.VITE_REST_API;
+  const REST_API = import.meta.env.VITE_REST_API_KEY;
   const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
 
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const handleLogin = () => {
+    const code = new URL(window.location.href).searchParams.get('code');
     window.location.href = kakaoURL;
+
+    return code;
   };
 
   return (
