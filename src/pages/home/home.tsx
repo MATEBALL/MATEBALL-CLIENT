bce259e07be4ab91707fba39f711dcafd1121d5a
heesunee <heesun729@uos.ac.kr>
2025-07-08 03:55:58 +0900

    feat: 카드 배너 컴포넌트 구현 (#72)

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,11 @@
+import CardBanner from '@components/card/card-banner/card-banner';
+
 const Home = () => {
-  return <div>홈</div>;
+  return (
+    <div>
+      홈<CardBanner subText="경기일 선택하고" text="나랑 딱 맞는 직관 메이트 찾자!" />
+    </div>
+  );
 };
 
 export default Home;
