cdc659bd3e5feef2ab9744926f5cee6f56f21cab
heesunee <heesun729@uos.ac.kr>
2025-07-08 01:41:19 +0900

    fix: 로그인 페이지 login.tsx로 이동 (#67)

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,21 @@
+import Icon from '@components/icon/icon';
+import graphic from '@img/graphic.png';
+
 const Login = () => {
-  return <div>로그인페이지</div>;
+  return (
+    <div className="h-screen flex-col-center gap-[7.2rem]">
+      <h1 className="head_20_sb text-gray-black">당신의 직관 메이트를 찾아드립니다</h1>
+      <img src={graphic} alt="임시 이미지" className="h-[16rem] w-[16rem]" />
+      <button
+        type="button"
+        aria-label="카카오 로그인"
+        className="subhead_18_sb flex w-[34.3rem] cursor-pointer items-center rounded-[0.8rem] bg-kakao-brand px-[1.6rem] py-[1.2rem]"
+      >
+        <Icon name="ic-kakao-logo" width={2.4} height={2.4} className="shrink-0" />
+        <p className="flex-1">카카오 로그인</p>
+      </button>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -1,21 +1,5 @@
-import Icon from '@components/icon/icon';
-import graphic from '@img/graphic.png';
-
 const SignUp = () => {
-  return (
-    <div className="h-screen flex-col-center gap-[7.2rem]">
-      <h1 className="head_20_sb text-gray-black">당신의 직관 메이트를 찾아드립니다</h1>
-      <img src={graphic} alt="임시 이미지" className="h-[16rem] w-[16rem]" />
-      <button
-        type="button"
-        aria-label="카카오 로그인"
-        className="subhead_18_sb flex w-[34.3rem] cursor-pointer items-center rounded-[0.8rem] bg-kakao-brand px-[1.6rem] py-[1.2rem]"
-      >
-        <Icon name="ic-kakao-logo" width={2.4} height={2.4} className="shrink-0" />
-        <p className="flex-1">카카오 로그인</p>
-      </button>
-    </div>
-  );
+  return <div>회원가입페이지</div>;
 };
 
 export default SignUp;
