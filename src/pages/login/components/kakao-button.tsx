7ee6ec26d0d7bbf613f06fa25b1c9abf51232da2
heesunee <heesun729@uos.ac.kr>
2025-07-08 02:11:01 +0900

    feat: 카카오 redirect uri 함수 작성 (#67)

--- src/pages/login/components/kakao-button.tsx
+++ src/pages/login/components/kakao-button.tsx
@@ -1,8 +1,18 @@
 import Icon from '@components/icon/icon';
 
 const KakaoButton = () => {
+  const REST_API = import.meta.env.VITE_REST_API;
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
+
   return (
     <button
+      onClick={handleLogin}
       type="button"
       aria-label="카카오 로그인"
       className="subhead_18_sb flex w-[34.3rem] cursor-pointer items-center rounded-[0.8rem] bg-kakao-brand px-[1.6rem] py-[1.2rem]"
