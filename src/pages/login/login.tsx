5d2e6918377d252ab899b6de2d564ce3144cdee8
heesunee <heesun729@uos.ac.kr>
2025-07-08 01:57:41 +0900

    refactor: 카카오로그인 버튼 컴포넌트로 분리 (#67)

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,4 @@
 import Icon from '@components/icon/icon';
-<<<<<<< HEAD
 import { LOTTIE_PATH } from '@constants/lotties';
 import KakaoButton from '@pages/login/components/kakao-button';
 import { Lottie } from '@toss/lottie';
@@ -16,23 +15,6 @@ const Login = () => {
         </div>
       </div>
       <KakaoButton />
-=======
-import graphic from '@img/graphic.png';
-
-const Login = () => {
-  return (
-    <div className="h-screen flex-col-center gap-[7.2rem]">
-      <h1 className="head_20_sb text-gray-black">당신의 직관 메이트를 찾아드립니다</h1>
-      <img src={graphic} alt="임시 이미지" className="h-[16rem] w-[16rem]" />
-      <button
-        type="button"
-        aria-label="카카오 로그인"
-        className="subhead_18_sb flex w-[34.3rem] cursor-pointer items-center rounded-[0.8rem] bg-kakao-brand px-[1.6rem] py-[1.2rem]"
-      >
-        <Icon name="ic-kakao-logo" width={2.4} height={2.4} className="shrink-0" />
-        <p className="flex-1">카카오 로그인</p>
-      </button>
->>>>>>> cdc659b (fix: 로그인 페이지 login.tsx로 이동 (#67))
     </div>
   );
 };
