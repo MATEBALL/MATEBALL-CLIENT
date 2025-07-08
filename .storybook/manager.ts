0ae7eeeed7ae7995c40d295474ef37cf8f507694
bongtta <kce0419@naver.com>
2025-07-09 01:55:54 +0900

    style: 스타일 수정 (#76)

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,10 +4,7 @@ import { themes } from '@storybook/theming';
 addons.setConfig({
   theme: {
     ...themes.light,
-    brandTitle: 'MATEBALL Storybook',
-    brandUrl:
-      'https://686128be307ebf524cf1c24e-cmyvingnyo.chromatic.com/?path=/docs/introduction--docs',
+    brandTitle: 'MATEBALL MATEBALL Design System',
     brandImage: '/svgs/logo-gray.svg',
-    brandTarget: '_blank',
   },
 });
--- src/stories/foundations/logo/logo.stories.tsx
+++ src/stories/foundations/logo/logo.stories.tsx
@@ -14,19 +14,19 @@ type Story = StoryObj;
 export const Logo: Story = {
   render: () => (
     <div className="flex flex-col items-center gap-[2rem]">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center p-[rem]">
         <p className="head_20_sb mb-2 text-gray-black">LogoSymbol</p>
         <img src="/svgs/ic-favicon.svg" alt="favicon" width={150} height={150} />
       </div>
 
       <div className="flex flex-col items-center">
         <p className="head_20_sb mb-2 text-gray-black">Logo</p>
-        <Icon name="logo" width={15} height={7} className="bg-gray-black" />
+        <Icon name="logo" width={15} height={7} className="bg-gray-black p-[1rem]" />
       </div>
 
       <div className="flex flex-col items-center">
         <p className="head_20_sb mb-2 text-gray-black">LogoFooter</p>
-        <Icon name="logo-gray" width={15} height={7} />
+        <Icon name="logo-gray" width={15} height={7} className="p-[1rem]" />
       </div>
     </div>
   ),
