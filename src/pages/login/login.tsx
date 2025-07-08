863706b72086b5b44a93e3c8e6024386f4fe0636
heesunee <heesun729@uos.ac.kr>
2025-07-09 07:00:12 +0900

    feat: 카카오 로그인 api 연결 (#67)

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,11 +1,16 @@
-import graphic from '@img/graphic.png';
 import KakaoButton from '@pages/login/components/kakao-button';
+import { Lottie } from '@toss/lottie';
 
 const Login = () => {
   return (
-    <div className="h-screen flex-col-center gap-[7.2rem]">
-      <h1 className="head_20_sb text-gray-black">당신의 직관 메이트를 찾아드립니다</h1>
-      <img src={graphic} alt="임시 이미지" className="h-[16rem] w-[16rem]" />
+    <div className="h-screen flex-col-between px-[1.6rem] pt-[8rem] pb-[4.8rem]">
+      <div className="w-[29.5rem] flex-col-center">
+        <Lottie src="/assets/lottie/login.json" loop={true} />
+        <div className="flex-col-center">
+          <h2 className="title_24_sb text-gray-800">나랑 딱! 맞는</h2>
+          <h2 className="title_24_sb text-gray-800">직관 메이트와 연결되는 공간</h2>
+        </div>
+      </div>
       <KakaoButton />
     </div>
   );
--- src/shared/apis/instance.ts
+++ src/shared/apis/instance.ts
@@ -1,7 +1,8 @@
 import { BASE_URL } from '@constants/api';
+import { ROUTES } from '@routes/routes-config';
 import type { AxiosError } from 'axios';
 import axios from 'axios';
-import { RESPONSE_MESSAGE } from '../constants/response';
+import { HTTP_STATUS, RESPONSE_MESSAGE } from '../constants/response';
 import type { errorResponseTypes } from '../types/api';
 
 export const instance = axios.create({
@@ -12,11 +13,8 @@ export const instance = axios.create({
   withCredentials: true,
 });
 
-// TODO: interceptor 추후 수정 필요
 instance.interceptors.response.use(
   (response) => {
-    // TODO: 명세서 보고 바꾸기
-    // response.data: { status, message, data }
     return response.data.data;
   },
   (error: AxiosError) => {
@@ -29,5 +27,20 @@ instance.interceptors.response.use(
     }
 
     return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const statusCode = error.response?.status;
+
+    if (statusCode === HTTP_STATUS.UNAUTHORIZED) {
+      window.location.replace(ROUTES.LOGIN);
+    }
+
+    if (statusCode === HTTP_STATUS.FORBIDDEN) {
+      window.location.replace(ROUTES.SIGNUP);
+    }
   },
 );
