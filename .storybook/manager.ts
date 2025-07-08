d3fb3b583c38587ee398ba2085d590b5256576a0
bongtta <kce0419@naver.com>
2025-07-09 02:08:58 +0900

    chore: url 복구 (#76)

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -5,6 +5,9 @@ addons.setConfig({
   theme: {
     ...themes.light,
     brandTitle: 'MATEBALL Design System',
+    brandUrl:
+      'https://686128be307ebf524cf1c24e-mtahknuhso.chromatic.com/?path=/docs/introduction--docs',
     brandImage: '/svgs/logo-gray.svg',
+    brandTarget: '_self',
   },
 });
