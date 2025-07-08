573c50a7589bd18210be66feb01d15f671debfcf
heesunee <heesun729@uos.ac.kr>
2025-07-08 20:56:45 +0900

    refactor: 프로필 체크 스텝 컴포넌트로 분리 (#74)

---
+++ src/pages/sign-up/components/profile-check-step.tsx
@@ -0,0 +1,36 @@
+import Button from '@components/button/button';
+import { mockKaKaoData } from '@mocks/mockKakaoData';
+import ProfileCard from '@pages/sign-up/components/profile-card';
+import { AGE_LIMIT_MESSAGE, AGE_NOTICE, NOTICE } from '@pages/sign-up/constants/NOTICE';
+import { isAdult } from '@pages/sign-up/utils/age-calculate';
+
+const ProfileCheckStep = () => {
+  const data = mockKaKaoData;
+  const isValid = !isAdult(data.birthYear);
+
+  return (
+    <div className="h-screen flex-col-between gap-[4rem] px-[1.6rem] pt-[4rem] pb-[1.6rem]">
+      <div className="w-full flex-col gap-[4rem]">
+        <div>
+          <h1 className="title_24_sb text-gray-black">기본 정보를 확인해 주세요.</h1>
+          <p className="cap_14_m whitespace-pre-line text-gray-600">{NOTICE}</p>
+        </div>
+        <div className="w-full flex-col gap-[1.6rem] ">
+          <div className="w-full flex-row gap-[1.2rem]">
+            <ProfileCard title="생년" data={data.birthYear} aria-label="생년" isValid={isValid} />
+            <ProfileCard title="성별" data={data.gender} aria-label="성별" />
+          </div>
+
+          {isValid ? (
+            <p className="cap_12_m text-state-error">{AGE_LIMIT_MESSAGE}</p>
+          ) : (
+            <p className="cap_12_m whitespace-pre-line text-gray-500">{AGE_NOTICE}</p>
+          )}
+        </div>
+      </div>
+      <Button label="가입하기" className="w-full" ariaLabel="가입하기" disabled={isValid} />
+    </div>
+  );
+};
+
+export default ProfileCheckStep;
