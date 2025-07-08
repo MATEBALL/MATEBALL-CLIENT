9776dff6c948fe32b7dfe6f876d165e2b352c3d7
heesunee <heesun729@uos.ac.kr>
2025-07-09 07:00:12 +0900

    feat: 카카오 로그인 api 연결 (#67)

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,14 +1,12 @@
-import Icon from '@components/icon/icon';
-import { LOTTIE_PATH } from '@constants/lotties';
 import KakaoButton from '@pages/login/components/kakao-button';
 import { Lottie } from '@toss/lottie';
 
 const Login = () => {
+  return <div>로그인페이지</div>;
   return (
     <div className="h-screen flex-col-between px-[1.6rem] pt-[8rem] pb-[4.8rem]">
       <div className="w-[29.5rem] flex-col-center">
-        <Icon name="logo-gray" width={16.5} height={4.5} />
-        <Lottie src={LOTTIE_PATH.LOGIN} loop={true} />
+        <Lottie src="/assets/lottie/login.json" loop={true} />
         <div className="flex-col-center">
           <h2 className="title_24_sb text-gray-800">나랑 딱! 맞는</h2>
           <h2 className="title_24_sb text-gray-800">직관 메이트와 연결되는 공간</h2>
