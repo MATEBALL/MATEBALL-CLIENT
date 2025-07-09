cfaa6b1a3a11c660d64670f3cd6046c99851aaf5
bongtta <kce0419@naver.com>
2025-07-10 03:52:21 +0900

    style: 스타일 수정 (#92)

--- src/pages/onboarding/components/progress-bar.tsx
+++ src/pages/onboarding/components/progress-bar.tsx
@@ -10,7 +10,7 @@ const ProgressBar = ({ currentStep, totalSteps }: ProgressBarProps) => {
     <div className="px-[0.8rem] py-[1.6rem]">
       <div className="h-[0.6rem] w-full rounded-[16px] bg-gray-300 ">
         <div
-          className="h-[0.6rem] rounded-[16px] bg-main-900 transition-all duration-300 ease-in-out"
+          className="h-full rounded-[16px] bg-main-900 transition-all duration-300 ease-in-out"
           style={{ width: `${progressPercent}%` }}
         />
       </div>
