b767ddde825a0e94b0efeae82f1c65f379e255d0
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-07-10 06:17:10 +0900

    feat: query 패턴 세팅 (#84)
    * feat: api endpoint 상수 작성 (#82)
    
    * feat: 유저 도메인 쿼리들 작성 (#82)
    
    * feat: 카카오 인가코드 발급 api 작성 (#82)
    
    * chore: chip 관련 파일 임시 삭제 (#82)
    
    * fix: post 파일 삭제 (#82)
    
    * feat: 쿼리키 & https 함수들 수정 (#82)
    
    * fix: mutationOptions 수정 (#82)
    
    ---------
    
    Co-authored-by: heesunee <heesun729@uos.ac.kr>

--- src/shared/apis/http.ts
+++ src/shared/apis/http.ts
@@ -1,21 +1,24 @@
 import { instance } from './instance';
-
-export function get<T>(...args: Parameters<typeof instance.get>) {
-  return instance.get<T>(...args);
+export async function get<T>(...args: Parameters<typeof instance.get>): Promise<T> {
+  const res = await instance.get<T>(...args);
+  return res.data;
 }
 
-export function post<T>(...args: Parameters<typeof instance.post>) {
-  return instance.post<T>(...args);
+export async function post<T>(...args: Parameters<typeof instance.post>): Promise<T> {
+  const res = await instance.post<T>(...args);
+  return res.data;
 }
 
-export function put<T>(...args: Parameters<typeof instance.put>) {
-  return instance.put<T>(...args);
+export async function put<T>(...args: Parameters<typeof instance.put>): Promise<T> {
+  const res = await instance.put<T>(...args);
+  return res.data;
 }
 
-export function patch<T>(...args: Parameters<typeof instance.patch>) {
-  return instance.patch<T>(...args);
+export async function patch<T>(...args: Parameters<typeof instance.patch>): Promise<T> {
+  const res = await instance.patch<T>(...args);
+  return res.data;
 }
-
-export function del<T>(...args: Parameters<typeof instance.delete>) {
-  return instance.delete<T>(...args);
+export async function del<T>(...args: Parameters<typeof instance.delete>): Promise<T> {
+  const res = await instance.delete<T>(...args);
+  return res.data;
 }
