96daa95ec0c5f5a7ba0e6b64529b0e0925b86967
yeeeww <yeangsshi@ewhain.net>
2025-07-09 18:42:42 +0900

    feat: home 페이지 기본 뷰 구현(#83)

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,15 @@
+import HomeCalendarTap from '@pages/home/components/home-calendar-tap';
+import HomeCardList from '@pages/home/components/home-card-list';
+import HomeTopContent from '@pages/home/components/home-top-content';
+
 const Home = () => {
-  return <div>홈</div>;
+  return (
+    <div className="pb-[5.6rem]">
+      <HomeTopContent />
+      <HomeCalendarTap />
+      <HomeCardList />
+    </div>
+  );
 };
 
 export default Home;
--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { getHeaderContent } from '@components/header/utils/get-header';
+import { ROUTES } from '@routes/routes-config';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header = () => {
@@ -6,9 +7,12 @@ const Header = () => {
   const location = useLocation();
   const urlParams = new URLSearchParams(location.search);
   const isFail = urlParams.has('fail');
+  const isHome = location.pathname === ROUTES.HOME;
 
   return (
-    <header className="h-[5.6rem] py-[1.55rem] pl-[2rem]">
+    <header
+      className={`sticky top-0 z-20 h-[5.6rem] py-[1.55rem] pl-[2rem] transition-colors duration-200 ${isHome ? 'bg-black' : 'none'} `}
+    >
       {getHeaderContent(location.pathname, isFail, navigate)}
     </header>
   );
