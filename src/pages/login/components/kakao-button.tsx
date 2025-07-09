9ea1b0831e4314dbe01e6d6386e1de041f0cf0d9
heesunee <heesun729@uos.ac.kr>
2025-07-09 17:13:51 +0900

    feat: 카카오 로그인 로티 적용 (#67)

--- src/pages/login/components/kakao-button.tsx
+++ src/pages/login/components/kakao-button.tsx
@@ -15,9 +15,9 @@ const KakaoButton = () => {
       onClick={handleLogin}
       type="button"
       aria-label="카카오 로그인"
-      className="subhead_18_sb flex w-[34.3rem] cursor-pointer items-center rounded-[0.8rem] bg-kakao-brand px-[1.6rem] py-[1.2rem]"
+      className="subhead_18_sb flex w-full cursor-pointer items-center rounded-[0.8rem] bg-kakao-brand px-[1.6rem] py-[1.2rem]"
     >
-      <Icon name="ic-kakao-logo" width={2.4} height={2.4} className="shrink-0" />
+      <Icon name="ic-kakao-logo" className="shrink-0" />
       <p className="flex-1">카카오 로그인</p>
     </button>
   );
