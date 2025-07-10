bd2cfe265b15008371dffc73d04c3c66d3a8816d
bongtta <kce0419@naver.com>
2025-07-11 01:39:25 +0900

    feat: 완료 스텝 퍼블리싱 (#98)

---
+++ src/pages/onboarding/components/complete.tsx
@@ -0,0 +1,24 @@
+import { LOTTIE_PATH } from '@constants/lotties';
+import { Lottie } from '@toss/lottie';
+
+const Complete = () => {
+  return (
+    <div className="flex-col-center gap-[4rem]">
+      <p className="title_24_sb text-center text-gray-black">
+        매칭 조건 설정이
+        <br />
+        모두 완료되었어요.
+      </p>
+
+      <Lottie src={LOTTIE_PATH.AGREE} loop={true} width={160} height={160} />
+
+      <p className="body_16_m text-center text-gray-600">
+        날짜를 선택해 맞춤 매칭을 생성하거나
+        <br />
+        나와 딱 맞는 메이트를 찾아 보세요.
+      </p>
+    </div>
+  );
+};
+
+export default Complete;
