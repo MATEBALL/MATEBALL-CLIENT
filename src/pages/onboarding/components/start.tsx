ba4ddb0469c2a1436077677d8cab66f766c83bfd
bongtta <kce0419@naver.com>
2025-07-11 05:00:22 +0900

    chore: 상수 추가 (#98)

--- src/pages/onboarding/components/start.tsx
+++ src/pages/onboarding/components/start.tsx
@@ -4,7 +4,7 @@ import { Lottie } from '@toss/lottie';
 const Start = () => {
   return (
     <div className="flex-col-between gap-[4rem]">
-      <p className="title_24_sb text-gray-black">환영합니다!</p>
+      <p className="title_24_sb p-[4rem] text-gray-black">환영합니다!</p>
       <Lottie src={LOTTIE_PATH.WELCOME} loop={true} width={160} height={160} />
       <p className="body_16_m text-center text-gray-600">
         원하는 조건을 선택하고
--- src/shared/constants/header.ts
+++ src/shared/constants/header.ts
@@ -1,9 +1,15 @@
 import { ROUTES } from '@routes/routes-config';
 
 export const NO_HEADER_PATHS = [
+  
   ROUTES.MATCH_CREATE,
+ 
   ROUTES.LOGIN,
+ 
   ROUTES.ERROR,
+ 
   ROUTES.LOADING,
   ROUTES.SIGNUP,
+,
+  ROUTES.ONBOARDING,
 ];
