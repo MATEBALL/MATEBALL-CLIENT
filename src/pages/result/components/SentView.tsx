e3417e0cff8beb529d86d17c0656b91d9adf5e29
Dubabbi <thdms3160@naver.com>
2025-07-11 01:46:48 +0900

    feat: 버튼 레이어 수정 (#103)

--- src/pages/result/components/SentView.tsx
+++ src/pages/result/components/SentView.tsx
@@ -15,7 +15,7 @@ export default function SentView() {
         모든 그룹원이 수락하면 그룹원이 됩니다. <br />
         ‘매칭 현황’에서 실시간으로 확인할 수 있어요.
       </p>
-      <div className="fixed bottom-0 left-1/2 w-full max-w-[430px] -translate-x-1/2 bg-gray-white px-[1.6rem] pb-[2.4rem] pt-[1.6rem] flex-row-center gap-[0.8rem]">
+      <div className="fixed bottom-0 left-1/2 w-full max-w-[430px] -translate-x-1/2 p-[1.6rem] flex-row-center gap-[0.8rem]">
         <Button label="메이트 더 찾아보기" onClick={() => navigate(-1)} />
         <Button label="매칭 현황 보기" onClick={() => navigate(ROUTES.MATCH)} />
       </div>
