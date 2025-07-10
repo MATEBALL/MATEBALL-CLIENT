77f90622f3687756ec631db84fb17310b7c5bfd3
bongtta <kce0419@naver.com>
2025-07-11 02:48:31 +0900

    feat: 온보딩 퍼널 구현 (#98)

--- src/pages/onboarding/onboarding.tsx
+++ src/pages/onboarding/onboarding.tsx
@@ -1,9 +1,15 @@
 import Button from '@components/button/button/button';
 import { useFunnel } from '@hooks/use-funnel';
 import { ROUTES } from '@routes/routes-config';
+import Complete from './components/complete';
+import Gender from './components/gender';
+import MatchingType from './components/matching-type';
 import ProgressBar from './components/progress-bar';
 import Start from './components/start';
-import { ONBOARDING_STEPS } from './constants/steps';
+import SupportTeam from './components/support-team';
+import SyncSupportTeam from './components/sync-support-team';
+import ViewingStyle from './components/viewing-style';
+import { ONBOARDING_STEPS } from './constants/onboarding';
 
 const Onboarding = () => {
   const { Funnel, Step, currentStep, currentIndex, steps, goNext, goPrev } = useFunnel(
@@ -11,22 +17,62 @@ const Onboarding = () => {
     ROUTES.HOME,
   );
   return (
-    <>
-      <ProgressBar currentStep={currentIndex + 1} totalSteps={steps.length} />
+    <div className="h-full flex-col-between gap-[1.6rem]">
+      <div className="w-full">
+        <ProgressBar currentStep={currentIndex + 1} totalSteps={steps.length} />
+      </div>
+
       <Funnel>
         <Step name="START">
-          <div className="flex flex-col justify-between">
-            <Start />
-            <div className="fixed bottom-0 w-full p-[1.6rem] ">
-              <Button label="시작하기" size={'L'} variant={'blue'} />
-            </div>
+          <Start />
+          <div className="sticky bottom-0 w-full p-[1.6rem]">
+            <Button label="시작하기" size={'L'} variant={'blue'} onClick={goNext} />
           </div>
         </Step>
-        <Step name="support_team">
-          <Button label="다음으로" />
+
+        <Step name="SUPPORT_TEAM">
+          <SupportTeam />
+          <div className="sticky bottom-0 w-full p-[1.6rem]">
+            <Button label="다음으로" size={'L'} variant={'blue'} onClick={goNext} />
+          </div>
+        </Step>
+
+        <Step name="SYNC_SUPPORT_TEAM">
+          <SyncSupportTeam />
+          <div className="sticky bottom-0 w-full p-[1.6rem]">
+            <Button label="다음으로" size={'L'} onClick={goNext} />
+          </div>
+        </Step>
+
+        <Step name="VIEWING_STYLE">
+          <ViewingStyle />
+          <div className="sticky bottom-0 w-full p-[1.6rem]">
+            <Button label="다음으로" size={'L'} onClick={goNext} />
+          </div>
+        </Step>
+
+        <Step name="GENDER">
+          <Gender />
+          <div className="sticky bottom-0 w-full p-[1.6rem]">
+            <Button label="다음으로" size={'L'} onClick={goNext} />
+          </div>
+        </Step>
+
+        <Step name="MATCHING_TYPE">
+          <MatchingType />
+          <div className="sticky bottom-0 w-full p-[1.6rem]">
+            <Button label="다음으로" size={'L'} onClick={goNext} />
+          </div>
+        </Step>
+
+        <Step name="COMPLETE">
+          <Complete />
+          <div className="sticky bottom-0 w-full p-[1.6rem]">
+            <Button label="메인 화면으로 이동하기" size={'L'} onClick={goNext} />
+          </div>
         </Step>
       </Funnel>
-    </>
+    </div>
   );
 };
 
