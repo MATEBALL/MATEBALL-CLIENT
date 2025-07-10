0b9f5a3bf638cc89a4e9ec1c6471aaf73ac5b5d6
Dubabbi <thdms3160@naver.com>
2025-07-11 00:22:01 +0900

    refactor: 커스텀 유틸 적용 (#59)

--- src/shared/components/bottom-sheet/bottom-sheet-modal.tsx
+++ src/shared/components/bottom-sheet/bottom-sheet-modal.tsx
@@ -8,22 +8,23 @@ interface BottomSheetModalProps {
   subDescription: string;
 }
 
-const BottomSheetModal = ({ isOpen, onClose, description, subDescription }: BottomSheetModalProps) => {
+const BottomSheetModal = ({
+  isOpen,
+  onClose,
+  description,
+  subDescription,
+}: BottomSheetModalProps) => {
   return (
     <BottomSheet showIndicator={false} isOpen={isOpen} onClose={onClose}>
-      <div className="mx-auto flex flex-col items-center justify-center gap-[0.4rem] px-[5rem] pt-[3.2rem]">
-        <div className="flex flex-col items-center gap-[1.6rem] self-stretch text-center">
-          <div className="title_24_sb text-center text-gray-black">잠깐!</div>
-          <div className="body_16_b text-center text-gray-900">
-            {description}
-          </div>
+      <div className="mx-auto flex-col-center gap-[0.4rem] px-[5rem] pt-[3.2rem] text-center">
+        <div className="flex-col-center gap-[1.6rem] self-stretch">
+          <div className="title_24_sb text-gray-black">잠깐!</div>
+          <div className="body_16_b text-gray-900">{description}</div>
         </div>
-        <div className="body_16_m text-center text-gray-800">
-          {subDescription}
-        </div>
+        <div className="body_16_m text-gray-800">{subDescription}</div>
       </div>
 
-      <div className="flex w-full items-center justify-center gap-[0.8rem] p-[1.6rem]">
+      <div className="w-full flex-row-center gap-[0.8rem] p-[1.6rem]">
         <Button label="다음에 할래요" className="bg-main-200 text-main-900" onClick={onClose} />
         <Button
           label="요청할래요"
