dacc4085281fb2874e630bcb39cd5a4846f72a49
Dubabbi <thdms3160@naver.com>
2025-07-11 02:07:15 +0900

    feat: 바텀시트 애니메이션 추가 (#59)

--- src/shared/components/bottom-sheet/bottom-sheet.tsx
+++ src/shared/components/bottom-sheet/bottom-sheet.tsx
@@ -1,6 +1,7 @@
 import BottomSheetIndicator from '@components/bottom-sheet/bottom-sheet-indicator';
 import useOutsideClick from '@components/bottom-sheet/hooks/use-outside-click';
 import { cn } from '@libs/cn';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useRef } from 'react';
 import { createPortal } from 'react-dom';
 
@@ -24,23 +25,41 @@ const BottomSheet = ({
   const sheetRef = useRef<HTMLDivElement>(null);
   useOutsideClick(sheetRef, onClose);
 
-  if (!isOpen) return null;
-
   return createPortal(
-    <div className="fixed inset-0 flex items-end justify-center">
-      <div className="absolute inset-0 bg-overlay" />
-      <div
-        ref={sheetRef}
-        className={cn(
-          'relative flex w-full max-w-[43rem] flex-col justify-center rounded-tl-[12px] rounded-tr-[12px] bg-gray-white',
-          gap,
-          className,
-        )}
-      >
-        {showIndicator && <BottomSheetIndicator />}
-        {children}
-      </div>
-    </div>,
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="fixed inset-0 z-50 flex items-end justify-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <motion.div
+            className="absolute inset-0 bg-overlay"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={onClose}
+          />
+
+          <motion.div
+            ref={sheetRef}
+            className={cn(
+              'relative flex w-full max-w-[43rem] flex-col rounded-tl-[12px] rounded-tr-[12px] bg-gray-white',
+              gap,
+              className,
+            )}
+            initial={{ y: '100%' }}
+            animate={{ y: 0 }}
+            exit={{ y: '100%' }}
+            transition={{ type: 'tween', duration: 0.3 }}
+          >
+            {showIndicator && <BottomSheetIndicator />}
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
     document.body,
   );
 };
