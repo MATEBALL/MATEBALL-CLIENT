7333a93cda41edd1bab1fb17e53cb419a191a095
heesunee <heesun729@uos.ac.kr>
2025-07-11 02:27:20 +0900

    feat: https 인증서 설정을 위한 키 설정 (#105)

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import path, { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import svgSprite from '@pivanov/vite-plugin-svg-sprite';
@@ -10,7 +11,21 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+const keyPath = process.env.VITE_PEM_KEY_PATH || 'localhost+2-key.pem';
+const certPath = process.env.VITE_PEM_CERT_PATH || 'localhost+2.pem';
+
+const https =
+  process.env.CLOUDFLARE_ENV === 'true'
+    ? undefined
+    : {
+        key: fs.readFileSync(path.resolve(dirname, keyPath)),
+        cert: fs.readFileSync(path.resolve(dirname, certPath)),
+      };
+
 export default defineConfig({
+  server: {
+    https,
+  },
   plugins: [
     react(),
     svgSprite({
