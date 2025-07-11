776a7169db90eaf92fe6d87c0b7aed26fc204726
Dubabbi <thdms3160@naver.com>
2025-07-11 14:35:09 +0900

    style: 경기 선택 바텀시트 수정 (#111)

--- src/shared/components/bottom-sheet/game-match/game-match-bottom-sheet.tsx
+++ src/shared/components/bottom-sheet/game-match/game-match-bottom-sheet.tsx
@@ -13,7 +13,7 @@ const GameMatchBottomSheet = ({ isOpen, onClose }: GameMatchBottomSheetProps) =>
 
   return (
     <BottomSheet isOpen={isOpen} onClose={onClose} showIndicator={true} gap="gap-[1.6rem]">
-      <div className="w-full px-4 flex-col">
+      <div className="w-full flex-col">
         <div className="w-full flex-col px-[1.6rem] gap-[1.3rem]">
           <div className="body_16_m text-gray-black">7월 17일 화요일</div>
           <GameMatchList selectedIdx={selectedIdx} onSelect={setSelectedIdx} />
--- src/shared/components/bottom-sheet/game-match/game-match-footer.tsx
+++ src/shared/components/bottom-sheet/game-match/game-match-footer.tsx
@@ -4,11 +4,11 @@ import Icon from '@components/icon/icon';
 const GameMatchFooter = () => {
   return (
     <section className="w-full flex-col-center gap-[0.4rem]">
-      <div className="w-full flex-row-center gap-[0.8rem]">
+      <div className="flex-row-center gap-[0.8rem] text-gray-600">
         <Icon name="ic-caution" size={1.8} />
-        <span className="cap_12_m text-gray-600">하루에 한 경기만 매칭 생성이 가능해요.</span>
+        <span className="cap_12_m">하루에 한 경기만 매칭 생성이 가능해요.</span>
       </div>
-      <div className="p-[1.6rem]">
+      <div className="w-full p-[1.6rem]">
         <Button className="w-full" label="맞춤 매칭 생성하기" />
       </div>
     </section>
