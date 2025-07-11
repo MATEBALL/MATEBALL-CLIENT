93089413b1dfe6ee192fdc250bf69107f69cbb35
Dubabbi <thdms3160@naver.com>
2025-07-11 15:14:34 +0900

    feat: 경기 선택 바텀시트 스토리 작성

--- src/shared/components/bottom-sheet/bottom-sheet.stories.tsx
+++ src/shared/components/bottom-sheet/bottom-sheet.stories.tsx
@@ -2,6 +2,8 @@ import BottomSheet from '@components/bottom-sheet/bottom-sheet';
 import BottomSheetModal from '@components/bottom-sheet/bottom-sheet-modal';
 import useBottomSheet from '@components/bottom-sheet/hooks/use-bottom-sheet';
 import Button from '@components/button/button/button';
+import GameMatchBottomSheet from '@components/bottom-sheet/game-match/game-match-bottom-sheet'
+
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 const meta: Meta<typeof BottomSheet> = {
@@ -74,6 +76,32 @@ export const RequestWarning: Story = {
           onClose={close}
           description="그룹 매칭은 최대 2건까지 신청할 수 있어요."
           subDescription="단, 하루에 한 경기만 매칭이 성사되며 같은 날짜의 중복 매칭은 불가능해요!"
+        />
+      </div>
+    );
+  },
+};
+
+export const GameMatch: Story = {
+  render: () => {
+    const { isOpen, open, close } = useBottomSheet();
+
+    const mockGameDatas = [
+      { id: 1, awayTeam: 'LG', homeTeam: '두산', gameTime: '18:30', stadium: '잠실' },
+      { id: 2, awayTeam: 'SSG', homeTeam: 'NC', gameTime: '18:30', stadium: '문학' },
+      { id: 3, awayTeam: '한화', homeTeam: '키움', gameTime: '18:30', stadium: '대전' },
+      { id: 4, awayTeam: '롯데', homeTeam: 'KT', gameTime: '18:30', stadium: '사직' },
+      { id: 5, awayTeam: '삼성', homeTeam: 'KIA', gameTime: '18:30', stadium: '대구' },
+    ];
+
+    return (
+      <div className="fixed top-4 left-4 justify-center">
+        <Button className="w-full" label="게임 매치 바텀시트 열기" onClick={open} />
+        <GameMatchBottomSheet
+          isOpen={isOpen}
+          onClose={close}
+          date="2025/07/17"
+          gameSchedule={mockGameDatas}
         />
       </div>
     );
