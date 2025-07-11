681d27f0137db1e6052db8cf1a97cefef61edbf7
heesunee <heesun729@uos.ac.kr>
2025-07-12 06:32:04 +0900

    feat: 회원가입 뷰 완성 (#109)

--- src/pages/sign-up/components/nickname-step.tsx
+++ src/pages/sign-up/components/nickname-step.tsx
@@ -2,11 +2,13 @@ import Button from '@components/button/button/button';
 import Input from '@components/input/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
+  BIRTHYEAR_RULE_MESSAGE,
+  BIRTHYEAR_SUCCESS_MESSAGE,
   NICKNAME_RULE_MESSAGE,
   NICKNAME_SUCCESS_MESSAGE,
   NICKNAME_TITLE,
 } from '@pages/sign-up/constants/NOTICE';
-import { NICKNAME_PLACEHOLDER } from '@pages/sign-up/constants/validation';
+import { BIRTH_PLACEHOLDER, NICKNAME_PLACEHOLDER } from '@pages/sign-up/constants/validation';
 import { type NicknameFormValues, NicknameSchema } from '@pages/sign-up/schema/validation-schema';
 import { useForm } from 'react-hook-form';
 
@@ -16,37 +18,89 @@ const NicknameStep = () => {
     handleSubmit,
     formState: { errors, isValid },
     watch,
+    setValue,
   } = useForm<NicknameFormValues>({
     mode: 'onChange',
     resolver: zodResolver(NicknameSchema),
-    defaultValues: { nickname: '' },
+    defaultValues: { nickname: '', gender: undefined, birthYear: '' },
   });
 
   const nicknameValue = watch('nickname');
+  const birthYearValue = watch('birthYear');
+  const genderValue = watch('gender');
+
   const isNicknameValid = !errors.nickname && nicknameValue.length > 0;
+  const isBirthYearValid = !errors.birthYear && birthYearValue.length > 0;
 
   const onSubmit = (data: NicknameFormValues) => {
-    console.log(data.nickname);
+    console.log(data);
   };
 
-  const { onBlur, ref, ...inputProps } = register('nickname');
+  const { onBlur: onNicknameBlur, ref: nicknameRef, ...nicknameInputProps } = register('nickname');
+
+  const {
+    onBlur: onBirthYearBlur,
+    ref: birthYearRef,
+    ...birthYearInputProps
+  } = register('birthYear');
+
+  const handleGenderClick = (gender: '여성' | '남성') => {
+    setValue('gender', gender, { shouldValidate: true, shouldDirty: true });
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="h-full flex-col justify-between gap-[4rem]">
-      <div className="flex-col gap-[4rem]">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="h-full w-full flex-col justify-between gap-[4rem]"
+    >
+      <div className="w-full flex-col gap-[4rem]">
         <h1 className="title_24_sb whitespace-pre-line">{NICKNAME_TITLE}</h1>
-        <Input
-          placeholder={NICKNAME_PLACEHOLDER}
-          label="닉네임"
-          icon="ic-x"
-          defaultMessage={isNicknameValid ? NICKNAME_SUCCESS_MESSAGE : NICKNAME_RULE_MESSAGE}
-          validationMessage={errors.nickname?.message}
-          isError={!!errors.nickname}
-          isValid={isNicknameValid}
-          onBlur={onBlur}
-          ref={ref}
-          {...inputProps}
-        />
+        <div className=" flex-col gap-[2.4rem]">
+          <Input
+            placeholder={NICKNAME_PLACEHOLDER}
+            label="닉네임"
+            icon="ic-x"
+            defaultMessage={isNicknameValid ? NICKNAME_SUCCESS_MESSAGE : NICKNAME_RULE_MESSAGE}
+            validationMessage={errors.nickname?.message}
+            isError={!!errors.nickname}
+            isValid={isNicknameValid}
+            onBlur={onNicknameBlur}
+            ref={nicknameRef}
+            {...nicknameInputProps}
+          />
+          <Input
+            placeholder={BIRTH_PLACEHOLDER}
+            label="생년"
+            icon="ic-x"
+            defaultMessage={isBirthYearValid ? BIRTHYEAR_SUCCESS_MESSAGE : BIRTHYEAR_RULE_MESSAGE}
+            validationMessage={errors.birthYear?.message}
+            isError={!!errors.birthYear}
+            isValid={isBirthYearValid}
+            onBlur={onBirthYearBlur}
+            ref={birthYearRef}
+            {...birthYearInputProps}
+          />
+          <div className="flex-col gap-[0.8rem]">
+            <p className="body_16_m">성별</p>
+            <div className="flex w-full gap-[0.8rem]">
+              <Button
+                variant={genderValue === '여성' ? 'skyblueBorder' : 'white'}
+                label="여성"
+                className="w-full bg-background"
+                onClick={() => handleGenderClick('여성')}
+              />
+              <Button
+                variant={genderValue === '남성' ? 'skyblueBorder' : 'white'}
+                label="남성"
+                className="w-full bg-background"
+                onClick={() => handleGenderClick('남성')}
+              />
+            </div>
+            {!!errors.gender && (
+              <p className="mt-1 text-red-500 text-sm">{errors.gender.message}</p>
+            )}
+          </div>
+        </div>
       </div>
       <Button
         label="가입하기"
--- src/pages/sign-up/schema/validation-schema.ts
+++ src/pages/sign-up/schema/validation-schema.ts
@@ -1,4 +1,7 @@
 import {
+  BIRTH_ERROR_MESSAGES,
+  GENDER_ERROR_MESSAGES,
+  GENDER_OPTIONS,
   NICKNAME_ERROR_MESSAGES,
   NICKNAME_MAX_LENGTH,
   NICKNAME_MIN_LENGTH,
@@ -26,6 +29,14 @@ export const NicknameSchema = z.object({
         message: NICKNAME_ERROR_MESSAGES.MIXEDLANG,
       },
     ),
+  birthYear: z
+    .string()
+    .min(4, { message: BIRTH_ERROR_MESSAGES.LENGTH })
+    .max(4, { message: BIRTH_ERROR_MESSAGES.LENGTH })
+    .regex(/^\d+$/, { message: BIRTH_ERROR_MESSAGES.NUMBER }),
+  gender: z.enum(GENDER_OPTIONS, {
+    required_error: GENDER_ERROR_MESSAGES.REQUIRED,
+  }),
 });
 
 export type NicknameFormValues = z.infer<typeof NicknameSchema>;
