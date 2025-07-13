b7e80a5419341417a5c1d14fd0afe2767b4ea5aa
yeeeww <yeangsshi@ewhain.net>
2025-07-14 01:22:45 +0900

    refactor: 로딩중 삭제 및 타입 단언 삭제(#117)

--- src/pages/match/create/components/match-card-section.tsx
+++ src/pages/match/create/components/match-card-section.tsx
@@ -1,7 +1,7 @@
 import Card from '@components/card/match-card/card';
 import type { CardProps } from '@components/card/match-card/types/card';
 import { formatToKoreanDate } from '@pages/home/utils/date-format';
-import type { MatchCardData } from '@pages/match/hooks/useMateCreate';
+import type { MatchCardData } from '@pages/match/hooks/use-mate-create';
 
 interface MatchCardSectionProps {
   matchData: MatchCardData;
--- src/pages/match/create/create.tsx
+++ src/pages/match/create/create.tsx
@@ -1,28 +1,17 @@
-import Loading from '@pages/loading/loading';
 import { isInvalidMatchId } from '@pages/match/constants/matching';
 import ButtonSection from '@pages/match/create/components/button-section';
 import Header from '@pages/match/create/components/header';
 import MatchCardSection from '@pages/match/create/components/match-card-section';
-import useMatchCreate, { type MatchCardData } from '@pages/match/hooks/useMateCreate';
+import useMatchCreate from '@pages/match/hooks/use-mate-create';
 import { ROUTES } from '@routes/routes-config';
 import { Navigate, useParams, useSearchParams } from 'react-router-dom';
 
 const Create = () => {
   const [searchParams] = useSearchParams();
-  const matchType = (searchParams.get('type') || 'single') as 'single' | 'group';
+  const matchType = (searchParams.get('type') as 'single' | 'group') || 'single';
   const { matchId } = useParams();
   const numericMatchId = Number(matchId) || 1;
-  const { matchData, loading } = useMatchCreate(numericMatchId, matchType);
-
-  if (loading) {
-    return (
-      <div className="h-full flex-col-center gap-[2.4rem] px-[1.6rem] pt-[9.6rem]">
-        <p className="body_16_m text-gray-600">
-          <Loading />
-        </p>
-      </div>
-    );
-  }
+  const { matchData } = useMatchCreate(numericMatchId, matchType);
 
   if (isInvalidMatchId(matchId?.toString())) {
     return <Navigate to={ROUTES.ERROR} replace />;
@@ -32,7 +21,7 @@ const Create = () => {
     <div className="h-full flex-col-between gap-[2.4rem] px-[1.6rem] pt-[9.6rem]">
       <main className="w-full flex-col-center gap-[4rem]">
         <Header nickname={matchData?.nickname || ''} />
-        <MatchCardSection matchData={matchData as MatchCardData} />
+        {matchData && <MatchCardSection matchData={matchData} />}
       </main>
       <ButtonSection matchType={matchType} />
     </div>
