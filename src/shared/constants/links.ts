b9cf3158ffc36a5639beae7d8c846c9052e76b62
bongtta <kce0419@naver.com>
2025-07-14 05:03:21 +0900

    feat: 외부 링크 추가 (#126)

---
+++ src/shared/constants/links.ts
@@ -0,0 +1,4 @@
+export const EXTERNAL_LINKS = {
+  PRIVACY_POLICY: 'https://www.notion.so/22bebc583aa580ce8c31eb0fbe84ac58?source=copy_link',
+  TERMS_OF_SERVICE: 'https://www.notion.so/22bebc583aa58015b71dd75cf7884d33?source=copy_link',
+} as const;
