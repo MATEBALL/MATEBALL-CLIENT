9e890d6eeb9086f0b863d530a3cc777eb3565246
heesunee <heesun729@uos.ac.kr>
2025-07-14 06:43:53 +0900

    style: 회원가입 페이지 svg있는 컴포넌트로 변경 (#135)

--- src/pages/sign-up/components/signup-step.tsx
+++ src/pages/sign-up/components/signup-step.tsx
@@ -86,8 +86,7 @@ const SignupStep = () => {
                 label="여성"
                 icon="ic-female"
                 iconSize="24"
-                iconPosition="right"
-                className="w-full bg-background"
+                className="flex w-full gap-[0.4rem] bg-background"
                 onClick={() => handleGenderClick('여성')}
               />
               <Button
@@ -95,8 +94,7 @@ const SignupStep = () => {
                 label="남성"
                 icon="ic-male"
                 iconSize="24"
-                iconPosition="right"
-                className="w-full bg-background"
+                className="flex w-full gap-[0.4rem] bg-background"
                 onClick={() => handleGenderClick('남성')}
               />
             </div>
