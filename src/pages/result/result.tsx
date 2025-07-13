6386d31e09a8d597aa289ea808250c9198fbef56
Dubabbi <thdms3160@naver.com>
2025-07-13 16:24:15 +0900

    feat: 매칭 요청 받은 화면 추가 (#113)

--- src/pages/result/result.tsx
+++ src/pages/result/result.tsx
@@ -2,6 +2,7 @@ import MatchingAgreeView from '@pages/result/components/matching-agree-view';
 import MatchingFailView from '@pages/result/components/matching-fail-view';
 import MatchingReceiveView from '@pages/result/components/matching-receive-view';
 import MatchingSuccessView from '@pages/result/components/matching-success-view';
+import MatchingReceiveView from '@pages/result/components/matching-receive-view';
 import SentView from '@pages/result/components/sent-view';
 import { ROUTES } from '@routes/routes-config';
 import { Navigate, useSearchParams } from 'react-router-dom';
