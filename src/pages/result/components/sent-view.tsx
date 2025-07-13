275b32e39e157941727594a3314d539d8f6c636e
Dubabbi <thdms3160@naver.com>
2025-07-14 02:18:45 +0900

    feat: 매칭 결과 이동 로직 추가 (#113)

--- src/pages/result/components/sent-view.tsx
+++ src/pages/result/components/sent-view.tsx
@@ -1,23 +1,22 @@
 import Button from '@components/button/button/button';
 import { LOTTIE_PATH } from '@constants/lotties';
 import { MATCHING_COMPLETE_MESSAGE } from '@pages/match/constants/matching';
-import { ROUTES } from '@routes/routes-config';
+import { useNavigationHandlers } from '@pages/result/constants/result-navigation';
 import { Lottie } from '@toss/lottie';
-import { useNavigate } from 'react-router-dom';
 
 interface SentViewProps {
   isGroupMatching?: boolean;
 }
 
 const SentView = ({ isGroupMatching = true }: SentViewProps) => {
-  const navigate = useNavigate();
+  const { handleGoHome, handleGoMatch } = useNavigationHandlers();
 
   return (
     <div className="h-full flex-col-between">
       <section className="flex-col-center gap-[4rem] pt-[4rem]">
         <h2 className="title_24_sb">매칭 요청이 전송되었어요!</h2>
         <div className="h-[16rem] w-[16rem] flex-row-center">
-          <Lottie src={LOTTIE_PATH.SEND} loop={true} className="width-[16rem]" />
+          <Lottie src={LOTTIE_PATH.SEND} loop={true} className="w-[16rem]" />
         </div>
         <p className="body_16_m text-center text-gray-600">
           {isGroupMatching ? MATCHING_COMPLETE_MESSAGE.group : MATCHING_COMPLETE_MESSAGE.single}
@@ -26,8 +25,8 @@ const SentView = ({ isGroupMatching = true }: SentViewProps) => {
         </p>
       </section>
       <div className="flex-row-center gap-[0.8rem] p-[1.6rem]">
-        <Button label="메이트 더 찾아보기" variant="skyblue" onClick={() => navigate(-1)} />
-        <Button label="매칭 현황 보기" onClick={() => navigate(ROUTES.MATCH)} />
+        <Button label="메이트 더 찾아보기" variant="skyblue" onClick={handleGoHome} />
+        <Button label="매칭 현황 보기" onClick={handleGoMatch} />
       </div>
     </div>
   );
