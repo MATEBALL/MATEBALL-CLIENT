644f37c9e35d7b552180e143a19d746277bd0a18
heesunee <heesun729@uos.ac.kr>
2025-07-14 05:06:24 +0900

    feat: protected route 작성 (#131)

--- src/shared/hooks/use-auth.ts
+++ src/shared/hooks/use-auth.ts
@@ -1,16 +1,16 @@
-import { userQueries } from '@apis/user/user-queries'
-import { useQuery } from '@tanstack/react-query'
+import { userQueries } from '@apis/user/user-queries';
+import { useQuery } from '@tanstack/react-query';
 
 const useAuth = () => {
-  const {data, isLoading, isError} = useQuery(userQueries.USER_INFO());
+  const { data, isLoading, isError } = useQuery(userQueries.USER_INFO());
 
   const isAuthenticated = !!data?.nickname;
 
-    return {
+  return {
     isAuthenticated,
     isLoading,
     isError,
   };
-}
+};
 
-export default useAuth
+export default useAuth;
