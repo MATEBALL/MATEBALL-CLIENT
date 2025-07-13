72c3e5e48879c965ebaca1b45e08c741e51d16bf
bongtta <kce0419@naver.com>
2025-07-14 03:04:22 +0900

    feat: 내 정보 페이지 퍼블리싱 (#126)

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,5 +1,25 @@
+import Button from '@components/button/button/button';
+import Card from '@components/card/match-card/card';
+import Footer from '@components/footer/footer';
+
 const Profile = () => {
-  return <div>마이페이지</div>;
+  return (
+    <div>
+      <div className="w-full flex-col-center gap-[1.6rem] px-[1.6rem] pt-[1.6rem] pb-[5.6rem]">
+        <Card
+          type="user"
+          nickname="김철수"
+          imgUrl={['/testprofile.jpeg']}
+          chips={['두산', '열정응원러']}
+          age="25"
+          gender="남성"
+          introduction="한 줄 소개 더미데이터"
+        />
+        <Button size={'L'} label="매칭 조건 재설정 하기" />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Profile;
