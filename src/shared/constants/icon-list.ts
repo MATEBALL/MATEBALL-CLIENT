a8a5ed266af6b5dde26461531fa369482a24f416
heesunee <heesun729@uos.ac.kr>
2025-07-13 23:04:44 +0900

    feat: 채팅방 카드 퍼블리싱 (#125)

--- src/shared/constants/icon-list.ts
+++ src/shared/constants/icon-list.ts
@@ -1,4 +1,3 @@
-
 export const iconNames = [
   'chat-profile',
   'ic-arrow-left-18',
@@ -29,11 +28,7 @@ export const iconNames = [
   'ic-x',
   'logo-gray',
   'logo',
-<<<<<<< HEAD
   'profile',
-=======
-  'profile'
->>>>>>> a1896fe (fix: 파일 경로들 수정 (#125))
 ] as const;
- 
+
 export type IconName = (typeof iconNames)[number];
--- src/shared/routes/layout.tsx
+++ src/shared/routes/layout.tsx
@@ -1,7 +1,12 @@
 import BottomNavigation from '@components/bottom-navigation/bottom-navigation';
 import Footer from '@components/footer/footer';
 import Header from '@components/header/header';
+<<<<<<< HEAD
 import { NO_HEADER_PATHS, SHOW_BOTTOM_NAVIGATE_PATHS } from '@constants/header';
+=======
+import { NO_HEADER_PATHS } from '@constants/header';
+// import Loading from '@pages/loading/loading';
+>>>>>>> 953ff31 (feat: 채팅방 카드 퍼블리싱 (#125))
 import { ROUTES } from '@routes/routes-config';
 import { Suspense } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
@@ -11,6 +16,7 @@ const Layout = () => {
   const { pathname, search } = useLocation();
   const params = new URLSearchParams(search);
 
+<<<<<<< HEAD
   const isFail = pathname === ROUTES.RESULT && params.get('type') === 'fail';
 
   const showBottomNav = SHOW_BOTTOM_NAVIGATE_PATHS.includes(pathname);
@@ -21,6 +27,16 @@ const Layout = () => {
       <Suspense>
         {showHeader && <Header />}
         <main className="flex-grow">
+=======
+  const showBottomNav = [ROUTES.HOME, ROUTES.MATCH, ROUTES.CHAT, ROUTES.PROFILE].includes(pathname);
+  const showHeader = !NO_HEADER_PATHS.includes(pathname);
+
+  return (
+    <div className="flex h-screen flex-col">
+      {showHeader && <Header />}
+      <main className="flex-grow">
+        <Suspense fallback={<div />}>
+>>>>>>> 953ff31 (feat: 채팅방 카드 퍼블리싱 (#125))
           <Outlet />
         </main>
         {pathname === ROUTES.HOME && <Footer />}
