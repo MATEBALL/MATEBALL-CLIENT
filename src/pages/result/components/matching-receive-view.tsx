e3a737d5965023dd01c8f8af66e98b9d4056375c
Dubabbi <thdms3160@naver.com>
2025-07-13 16:46:40 +0900

    refactor: 포맷팅 적용 (#113)

--- src/pages/result/components/matching-receive-view.tsx
+++ src/pages/result/components/matching-receive-view.tsx
@@ -1,10 +1,10 @@
-import { useNavigate } from 'react-router-dom';
-import { cn } from '@libs/cn';
 import Button from '@components/button/button/button';
-import { MATCHING_HEADER_MESSAGE } from '@pages/result/constants/matching-result';
 import Card from '@components/card/match-card/card';
 import type { ChipColor } from '@components/card/match-card/types/card';
+import { cn } from '@libs/cn';
+import { MATCHING_HEADER_MESSAGE } from '@pages/result/constants/matching-result';
 import { ROUTES } from '@routes/routes-config';
+import { useNavigate } from 'react-router-dom';
 
 interface MatchingReceiveViewProps {
   isGroupMatching?: boolean;
@@ -28,9 +28,7 @@ const mockMate = {
   matchRate: 99,
 };
 
-const MatchingReceiveView = ({
-  isGroupMatching = true,
-}: MatchingReceiveViewProps) => {
+const MatchingReceiveView = ({ isGroupMatching = true }: MatchingReceiveViewProps) => {
   const navigate = useNavigate();
 
   const handleReject = () => {
@@ -45,9 +43,7 @@ const MatchingReceiveView = ({
     <div className="h-full flex-col-between overflow-hidden">
       <div className="flex-col-center gap-[4rem] pt-[4rem]">
         <section className="gap-[0.8rem] text-center">
-          <h1 className="title_24_sb text-gray-black">
-            {MATCHING_HEADER_MESSAGE.description}
-          </h1>
+          <h1 className="title_24_sb text-gray-black">{MATCHING_HEADER_MESSAGE.description}</h1>
           <p className="body_16_m text-gray-600">
             {isGroupMatching
               ? MATCHING_HEADER_MESSAGE.group.subDescription
@@ -60,8 +56,14 @@ const MatchingReceiveView = ({
         </div>
       </div>
 
-      <section className="flex-row-center w-full gap-[0.8rem] p-[1.6rem]">
-        <Button size="L" variant="skyblue" className="w-full" label="요청 거절하기" onClick={handleReject} />
+      <section className="w-full flex-row-center gap-[0.8rem] p-[1.6rem]">
+        <Button
+          size="L"
+          variant="skyblue"
+          className="w-full"
+          label="요청 거절하기"
+          onClick={handleReject}
+        />
         <Button size="L" className="w-full" label="요청 수락하기" onClick={handleAccept} />
       </section>
     </div>
