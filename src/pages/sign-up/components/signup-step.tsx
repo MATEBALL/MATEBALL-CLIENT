388fe69a842120d1d0a078d6095f8da026b43d91
Dubabbi <thdms3160@naver.com>
2025-07-15 05:38:24 +0900

    refactor: 누락된 icon name 추가 (#132)

--- src/pages/sign-up/components/signup-step.tsx
+++ src/pages/sign-up/components/signup-step.tsx
@@ -84,14 +84,14 @@ const SignupStep = () => {
               <Button
                 variant={genderValue === '여성' ? 'skyblueBorder' : 'white'}
                 label="여성"
-                icon="ic-female"
+                icon="female"
                 className="flex w-full gap-[0.4rem] bg-background"
                 onClick={() => handleGenderClick('여성')}
               />
               <Button
                 variant={genderValue === '남성' ? 'skyblueBorder' : 'white'}
                 label="남성"
-                icon="ic-male"
+                icon="male"
                 className="flex w-full gap-[0.4rem] bg-background"
                 onClick={() => handleGenderClick('남성')}
               />
