4d64061432793ea97ccbabe3752ebdcffa8e8bac
heesunee <heesun729@uos.ac.kr>
2025-07-14 21:22:58 +0900

    feat: game 도메인 타입 작성 (#148)

---
+++ src/shared/types/game-types.ts
@@ -0,0 +1,11 @@
+export interface getGameScheduleResponse {
+    gameSchedule: Game[];
+}
+
+export interface Game {
+    id: number;
+    awayTeam: string;
+    homeTeam: string;
+    gameTime: string;
+    stadium: string;
+}
