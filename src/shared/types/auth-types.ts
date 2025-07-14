62c301abfe280007369cf2d9f5ca94a5766fb1e1
heesunee <heesun729@uos.ac.kr>
2025-07-15 01:08:54 +0900

    feat: 사용자 상태 정보 반환 타입 추가 (#148)

--- src/shared/types/auth-types.ts
+++ src/shared/types/auth-types.ts
@@ -6,3 +6,12 @@ export interface getAuthLoginResponse {
   gender: string;
   birthyear: string;
 }
+
+/**
+ * 사용자 상태 정보 반환
+ */
+
+export interface getUserStatusResponse {
+  nickname: boolean;
+  condition: boolean;
+}
