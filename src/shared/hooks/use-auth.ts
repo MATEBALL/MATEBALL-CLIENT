6041acf8c7e352585df4189274349df215eae6b0
heesunee <heesun729@uos.ac.kr>
2025-07-15 03:52:46 +0900

    feat: authGuard 회원가입 분기 삭제 (#154)

--- src/shared/hooks/use-auth.ts
+++ src/shared/hooks/use-auth.ts
@@ -1,17 +1,20 @@
-import { userQueries } from '@apis/user/user-queries';
+import { authQueries } from '@apis/auth/auth';
 import { useQuery } from '@tanstack/react-query';
 
 const useAuth = () => {
-  const { data, isLoading, isError } = useQuery(userQueries.USER_INFO());
+  const { data, isLoading, isError } = useQuery(authQueries.USER_STATUS());
 
   const isAuthenticated = !!data?.nickname;
-  const needsSignup = data && data.nickname === null;
+
+  // TODO: 로그인한 유저가 홈에 왔는데 매칭 조건 설정 전이면 false
+  // 이 false 값을 가지고 홈 불투명한거 처리해주세요
+  const isNotMatched = data?.condition === false;
 
   return {
     isAuthenticated,
     isLoading,
     isError,
-    needsSignup,
+    isNotMatched,
   };
 };
 
