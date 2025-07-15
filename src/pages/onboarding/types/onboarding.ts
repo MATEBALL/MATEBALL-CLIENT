d1fa2ba63d725fdb1dedc612713c716ed2ccf6a0
bongtta <kce0419@naver.com>
2025-07-16 02:34:43 +0900

    chore: 필요없는 코드 삭제 (#170)

--- src/pages/onboarding/types/onboarding.ts
+++ src/pages/onboarding/types/onboarding.ts
@@ -2,7 +2,3 @@ export interface OnboardingStepProps {
   selectedOption: string | null;
   onSelect: (option: string) => void;
 }
-
-export interface MatchingTypeProps extends OnboardingStepProps {
-  selections: Record<string, string | null>;
-}
