fd4ebab9931e8d0ea2b482c4e7e0022b2a0e2183
heesunee <heesun729@uos.ac.kr>
2025-07-16 01:45:55 +0900

    fix: 회원가입 페이지 높이 짤림 수정 (#182)

--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -2,7 +2,7 @@ import SignupStep from '@pages/sign-up/components/signup-step';
 
 const SignUp = () => {
   return (
-    <div className="signup-layout bg-gray-white">
+    <div className="signup-layout h-screen bg-gray-white">
       <SignupStep />
     </div>
   );
