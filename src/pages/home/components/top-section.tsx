33e148162aa1d500075b6ecb1eda31083e4443c0
heesunee <heesun729@uos.ac.kr>
2025-07-16 00:10:44 +0900

    fix: global layout fix (#174)

--- src/pages/home/components/top-section.tsx
+++ src/pages/home/components/top-section.tsx
@@ -3,7 +3,7 @@ import CardBanner from '@components/card/banner-card/banner-card';
 const TopSection = () => {
   return (
     <section className="bg-gray-black px-[1.6rem]">
-      <section className="head_20_sb mb-[1.6rem] text-gray-white">당신을 위한 맞춤형 매칭</section>
+      <h2 className="head_20_sb mb-[1.6rem] text-gray-white">당신을 위한 맞춤형 매칭</h2>
       <CardBanner subText="경기일 선택하고" text="나랑 딱 맞는 직관 메이트 찾자!" />
     </section>
   );
