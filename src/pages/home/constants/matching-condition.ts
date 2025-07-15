d090da7fc33a3b569d274a911d6e48d376e69100
yeeeww <yeangsshi@ewhain.net>
2025-07-16 01:29:54 +0900

    feat: 최초 매칭 모달 연결 (#176)

---
+++ src/pages/home/constants/matching-condition.ts
@@ -0,0 +1,1 @@
+export const MATCHING_MODAL_DESCRIPTION = '어떤 메이트를 만나고 싶나요?\n' + '나와 딱 맞는 직관 메이트를\n'+'찾아드릴게요!';  
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,12 +2,18 @@ import { gameQueries } from '@apis/game/game-queries';
 import GameMatchBottomSheet from '@components/bottom-sheet/game-match/game-match-bottom-sheet';
 import { WEEK_CALENDAR_START_OFFSET } from '@components/calendar/constants/CALENDAR';
 import { getInitialSelectedDate } from '@components/calendar/utils/date-grid';
+import Dialog from '@components/dialog/dialog';
+import useAuth from '@hooks/use-auth';
 import { useTabState } from '@hooks/use-tab-state';
 import CalendarBottomSheet from '@pages/home/components/calendar-bottom-sheet';
 import CalendarSection from '@pages/home/components/calendar-section';
 import MatchListSection from '@pages/home/components/match-list-section';
 import TopSection from '@pages/home/components/top-section';
+<<<<<<< HEAD
 import { useQuery } from '@tanstack/react-query';
+=======
+import { MATCHING_MODAL_DESCRIPTION } from '@pages/home/constants/matching-condition';
+>>>>>>> 03372e9 (feat: 최초 매칭 모달 연결 (#176))
 import { addDays, format } from 'date-fns';
 import { useState } from 'react';
 
@@ -22,8 +28,11 @@ const Home = () => {
   );
   const [isCalendarBottomSheetOpen, setIsCalendarBottomSheetOpen] = useState(false);
   const [isGameInfoBottomSheetOpen, setIsGameInfoBottomSheetOpen] = useState(false);
+
   const dateStr = format(selectedDate, 'yyyy-MM-dd');
   const { data } = useQuery(gameQueries.GAME_LIST(dateStr));
+  const { needsMatchingSetup } = useAuth();
+
   const handleDateSelect = (date: Date) => {
     setSelectedDate(date);
     setBaseWeekDate(date);
@@ -61,6 +70,11 @@ const Home = () => {
         gameSchedule={data ?? []}
         activeType={activeType}
       />
+      {needsMatchingSetup && (
+        <div className="matching-modal-backdrop z-[var(--z-modal)] flex-col-center ">
+          <Dialog info={MATCHING_MODAL_DESCRIPTION} />
+        </div>
+      )}
     </div>
   );
 };
