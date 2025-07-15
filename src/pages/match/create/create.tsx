6933cee9979456b1ee5bb41bca07caec3446f046
yeeeww <yeangsshi@ewhain.net>
2025-07-16 06:27:50 +0900

    feat: 매칭 생성 api 연동 중 (#197)

--- src/pages/match/create/create.tsx
+++ src/pages/match/create/create.tsx
@@ -1,9 +1,11 @@
+import { matchQueries } from '@apis/match/match-queries';
 import ButtonSection from '@pages/match/create/components/button-section';
 import MatchCardSection from '@pages/match/create/components/match-card-section';
 import MatchGuideSection from '@pages/match/create/components/match-guide-section';
 import useMatchCreate from '@pages/match/hooks/use-mate-create';
 import { isInvalidMatchId } from '@pages/match/utils/match-validators';
 import { ROUTES } from '@routes/routes-config';
+//import { useQuery } from '@tanstack/react-query';
 import { Navigate, useParams, useSearchParams } from 'react-router-dom';
 
 const Create = () => {
@@ -19,6 +21,9 @@ const Create = () => {
   if (!matchData || isInvalidMatchId(matchId?.toString()) || !matchType) {
     return <Navigate to={ROUTES.ERROR} replace />;
   }
+  const id = 55;
+  //const { data } = useQuery(matchQueries.SINGLE_MATCH_RESULT(id));
+  //console.log(data);
 
   return (
     <div className="h-svh flex-col-between gap-[2.4rem] px-[1.6rem] pt-[9.6rem]">
