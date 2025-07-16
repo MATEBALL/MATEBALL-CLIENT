518c8b5771dde7af2af8e6dad093eee4a01e8290
heesunee <heesun729@uos.ac.kr>
2025-07-17 03:41:18 +0900

    docs: 코딩 컨벤션 문서 작성 (#214)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,19 +10,9 @@ export default defineConfig({
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-        ],
-      },
-    ],
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/MATEBALL/MATEBALL-CLIENT' }],
   },
 });
