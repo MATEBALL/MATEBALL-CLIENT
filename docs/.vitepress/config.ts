51ad81aedab5fd2ec82368322510b0e79da5903d
heesunee <heesun729@uos.ac.kr>
2025-07-17 04:47:42 +0900

    feat: workflow 파일 작성 (#214)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/MATEBALL-CLIENT/',
   title: 'MATEBALL CLIENT DOCS',
   description: 'mateball client convention & troubleshootig & docs',
 
