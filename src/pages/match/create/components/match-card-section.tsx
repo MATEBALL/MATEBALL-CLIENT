5c52921f808a275ff601ec201ce922814f9e074f
yeeeww <yeangsshi@ewhain.net>
2025-07-17 06:06:38 +0900

    chore: 리베이스 (#197)

--- src/pages/match/create/components/match-card-section.tsx
+++ src/pages/match/create/components/match-card-section.tsx
@@ -1,22 +1,21 @@
-import Card from '@components/card/match-card/card';
-import type { CardProps } from '@components/card/match-card/types/card';
-import { formatToKoreanDate } from '@pages/home/utils/date-format';
-import type { MatchCardData } from '@pages/match/hooks/use-mate-create';
 import GroupMatchCard from '@pages/match/create/components/group-match-card-section';
 import SingleMatchCard from '@pages/match/create/components/single-match-card-section';
 
 interface MatchCardSectionProps {
-  matchData: MatchCardData;
+  matchId: number;
+  type: 'single' | 'group';
 }
 
-const MatchCardSection = ({ matchData }: MatchCardSectionProps) => {
-  const cardProps: CardProps = {
-    ...matchData,
-    date: formatToKoreanDate(matchData.date),
-    className: 'w-full',
-  };
+const MatchCardSection = ({ matchId, type }: MatchCardSectionProps) => {
+  if (type === 'single') {
+    return <SingleMatchCard matchId={matchId} />;
+  }
 
-  return <Card className="w-full" {...cardProps} />;
+  if (type === 'group') {
+    return <GroupMatchCard matchId={matchId} />;
+  }
+
+  return null;
 };
 
 export default MatchCardSection;
--- src/pages/match/create/create.tsx
+++ src/pages/match/create/create.tsx
@@ -1,10 +1,5 @@
 import ButtonSection from '@pages/match/create/components/button-section';
 import MatchCardSection from '@pages/match/create/components/match-card-section';
-<<<<<<< HEAD
-import MatchGuideSection from '@pages/match/create/components/match-guide-section';
-import useMatchCreate from '@pages/match/hooks/use-mate-create';
-=======
->>>>>>> 8ec3b46 (feat: 빌드 에러 해결 (#197))
 import { isInvalidMatchId } from '@pages/match/utils/match-validators';
 import { ROUTES } from '@routes/routes-config';
 import { Navigate, useParams, useSearchParams } from 'react-router-dom';
@@ -17,18 +12,14 @@ const Create = () => {
     matchTypeParam === 'single' || matchTypeParam === 'group' ? matchTypeParam : undefined;
 
   const numericMatchId = Number(matchId);
-  const { matchData } = useMatchCreate(numericMatchId, matchType);
 
-  if (!matchData || isInvalidMatchId(matchId?.toString()) || !matchType) {
+  if (isInvalidMatchId(matchId?.toString()) || !matchType) {
     return <Navigate to={ROUTES.ERROR} replace />;
   }
 
   return (
     <div className="h-svh flex-col-between gap-[2.4rem] px-[1.6rem] pt-[9.6rem]">
-      <div className="w-full flex-col-center gap-[4rem]">
-        <MatchGuideSection nickname={matchData.nickname} />
-        <MatchCardSection matchData={matchData} />
-      </div>
+      <MatchCardSection matchId={numericMatchId} type={matchType} />
       <ButtonSection matchType={matchType} />
     </div>
   );
