fd990f4ea1470713689d1f3ae9a385cf0160921c
Yewon Kim <163109964+yeeeww@users.noreply.github.com>
2025-07-18 07:13:49 +0900

    fix: (QA/2) 매칭 생성 로딩 중 수정 (#246)
    * fix: 로딩중 create 페이지에 추가 (#244)
    
    * fix: 바텀시트 스타일 수정 (#244)

--- src/shared/components/bottom-sheet/game-match/game-match-bottom-sheet.tsx
+++ src/shared/components/bottom-sheet/game-match/game-match-bottom-sheet.tsx
@@ -1,13 +1,13 @@
 import { matchMutations } from '@apis/match/match-mutations';
+import { matchQueries } from '@apis/match/match-queries';
 import BottomSheet from '@components/bottom-sheet/bottom-sheet';
 import GameMatchFooter from '@components/bottom-sheet/game-match/game-match-footer';
 import GameMatchList from '@components/bottom-sheet/game-match/game-match-list';
 import { formatDateWeekday } from '@components/bottom-sheet/game-match/utils/format-date-weekday';
 import { TAB_TYPES, type TabType } from '@components/tab/tab/constants/tab-type';
 import { MATCH_REQUEST_ERROR_MESSAGES } from '@constants/error-toast';
-import Loading from '@pages/loading/loading';
 import { ROUTES } from '@routes/routes-config';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -28,6 +28,7 @@ interface GameMatchBottomSheetProps {
   gameSchedule: GameScheduleItem[];
   onClick?: (selectedId: number | null) => void;
   activeType: TabType;
+  fromOnboarding?: boolean;
 }
 
 const GameMatchBottomSheet = ({
@@ -36,19 +37,18 @@ const GameMatchBottomSheet = ({
   date,
   gameSchedule,
   activeType,
+  fromOnboarding = false,
 }: GameMatchBottomSheetProps) => {
   const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
-  const [isNavigating, setIsNavigating] = useState(false);
   const navigate = useNavigate();
-
+  const queryClient = useQueryClient();
   const createMatchMutation = useMutation(matchMutations.CREATE_MATCH());
 
-  const disabled = selectedIdx === null || createMatchMutation.isPending || isNavigating;
+  const disabled = selectedIdx === null || createMatchMutation.isPending;
   const matchType = activeType === TAB_TYPES.SINGLE ? 'direct' : 'group';
   const queryType = activeType === TAB_TYPES.SINGLE ? 'single' : 'group';
 
   const handleClose = () => {
-    if (isNavigating) return;
     setSelectedIdx(null);
     onClose();
   };
@@ -67,16 +67,25 @@ const GameMatchBottomSheet = ({
         onSuccess: (response) => {
           const createdMatchId = response.matchId.toString();
 
+          if (matchType === 'direct') {
+            queryClient.invalidateQueries({
+              queryKey: matchQueries.SINGLE_MATCH_LIST(date).queryKey,
+            });
+          } else {
+            queryClient.invalidateQueries({
+              queryKey: matchQueries.GROUP_MATCH_LIST(date).queryKey,
+            });
+          }
+
           handleClose();
 
-          setIsNavigating(true);
-
-          setTimeout(() => {
+          if (fromOnboarding) {
+            navigate(`${ROUTES.ONBOARDING_GROUP}?step=COMPLETE`);
+          } else {
             navigate(`${ROUTES.MATCH_CREATE(createdMatchId)}?type=${queryType}`);
-          }, 2000);
+          }
         },
         onError: (error) => {
-          setIsNavigating(false);
           const status = (error as AxiosError)?.response?.status;
 
           if (status === MATCH_REQUEST_ERROR_MESSAGES.TOO_MANY_REQUESTS.status) {
@@ -93,9 +102,6 @@ const GameMatchBottomSheet = ({
     );
   };
 
-  if (isNavigating) {
-    return <Loading />;
-  }
   return (
     <BottomSheet isOpen={isOpen} onClose={handleClose} showIndicator gap="gap-[1.6rem]">
       <div className="w-full flex-col">
