e157fd3255244675d189c6d8dc60fddf73a422ad
heesunee <heesun729@uos.ac.kr>
2025-07-17 20:42:07 +0900

    feat: COMPLETE 스텝 뒤로가기 제어 (#221)

--- src/pages/match/utils/mate.ts
+++ src/pages/match/utils/mate.ts
@@ -1,6 +1,7 @@
 import type { ChipColor } from '@components/card/match-card/types/card';
+import type { singleMatchMate } from '@/shared/types/match-types';
 
-export const mapMateData = (mate: any) => ({
+export const mapMateData = (mate: singleMatchMate) => ({
   ...mate,
   type: 'detailed',
   imgUrl: [mate.imgUrl],
