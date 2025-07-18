c4927c4d0104087a0acdb57e36ad3957c32c454d
bongtta <kce0419@naver.com>
2025-07-18 19:39:21 +0900

    refactor: isNavigating 상태 리셋 (#254)

--- src/shared/components/bottom-sheet/game-match/game-match-bottom-sheet.tsx
+++ src/shared/components/bottom-sheet/game-match/game-match-bottom-sheet.tsx
@@ -87,6 +87,7 @@ const GameMatchBottomSheet = ({
           }
         },
         onError: (error) => {
+          setIsNavigating(false);
           const status = (error as AxiosError)?.response?.status;
 
           if (status === MATCH_REQUEST_ERROR_MESSAGES.TOO_MANY_REQUESTS.status) {
