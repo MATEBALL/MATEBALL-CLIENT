9ab7667e06075b427f47938faf40e64eca95f6df
bongtta <kce0419@naver.com>
2025-07-31 16:53:11 +0900

    style: icon과 text 사이 간격 수정 (#301)

--- src/shared/utils/show-error-toast.tsx
+++ src/shared/utils/show-error-toast.tsx
@@ -4,11 +4,15 @@ import { toast } from 'react-compact-toast';
 export const showErrorToast = (message: string, offset: string = '8.3rem') => {
   toast({
     text: message,
-    icon: <Icon name="error" />,
-    autoClose: 3000,
+    icon: (
+      <div className="mr-[0.4rem]">
+        <Icon name="error" />
+      </div>
+    ),
+    autoClose: 300000,
     position: 'bottomCenter',
     offset,
     className:
-      'min-h-[4.5rem]! max-w-[calc(43rem-3.2rem)] w-[calc(100%-3.2rem)] cap_14_m text-gray-white rounded-[12px] gap-[0.8rem] bg-gray-900',
+      'min-h-[4.5rem]! max-w-[calc(43rem-3.2rem)] w-[calc(100%-3.2rem)] cap_14_m text-gray-white rounded-[12px] bg-gray-900',
   });
 };
