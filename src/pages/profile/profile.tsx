6dda0bb71d41b975729bdf34951e2c46d17b0d24
heesunee <heesun729@uos.ac.kr>
2025-08-25 13:42:50 +0900

    feat: 문의하기, 의견 보내기 구글폼 연결 (#305)

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -9,6 +9,10 @@ import { useQuery } from '@tanstack/react-query';
 const Profile = () => {
   const { data } = useQuery(userQueries.USER_INFO());
 
+  const handleClick = (link: string) => {
+    window.open(link, '_blank', 'noopener,noreferrer');
+  };
+
   if (!data) return null;
 
   return (
@@ -29,6 +33,28 @@ const Profile = () => {
         <Button size="L" label="프로필 · 매칭 조건 수정" />
       </div>
       <Divider thickness={0.4} color="bg-gray-200" />
+      <section className="w-full flex-col items-start px-[1.6rem]">
+        <button
+          type="button"
+          aria-label="문의하기"
+          className="cap_14_m py-[0.8rem] text-gray-800"
+          onClick={() => handleClick('https://notion')}
+        >
+          <p>문의하기</p>
+        </button>
+        <button
+          type="button"
+          aria-label="의견 보내기"
+          className="cap_14_m py-[0.8rem] text-gray-800"
+          onClick={() => handleClick('https://notion')}
+        >
+          <p>의견 보내기</p>
+        </button>
+        <Divider color="bg-gray-300" margin="my-[1.6rem]" />
+        <button type="button" aria-label="로그아웃" className="cap_14_m py-[0.8rem] text-gray-800">
+          <p>로그아웃</p>
+        </button>
+      </section>
       <Footer />
     </div>
   );
