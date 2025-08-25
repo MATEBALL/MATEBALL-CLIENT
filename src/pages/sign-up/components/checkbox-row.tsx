96be92bf08f5a349b9751ddea40b33a5fd2f08cb
heesunee <heesun729@uos.ac.kr>
2025-08-25 12:32:33 +0900

    feat: 체크박스 row 컴포넌트로 분리 (#303)

---
+++ src/pages/sign-up/components/checkbox-row.tsx
@@ -0,0 +1,34 @@
+import Icon from '@components/icon/icon';
+import { cn } from '@libs/cn';
+import type { ReactNode } from 'react';
+
+interface CheckboxProps {
+  label: ReactNode;
+  checked: boolean;
+  onClick: () => void;
+  svg?: ReactNode;
+  divider?: boolean;
+  className?: string;
+}
+
+const CheckboxRow = ({ label, checked, onClick, svg, divider, className }: CheckboxProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn(
+        'flex w-full items-center justify-between gap-[0.8rem] p-[0.8rem] px-[1.6rem] text-left',
+        divider && 'border-gray-200 border-b',
+        className,
+      )}
+    >
+      <div className="flex items-center gap-[0.8rem]">
+        <Icon name="check-filled" className={checked ? 'text-main-800' : 'text-gray-300'} />
+        <span className="body_16_m">{label}</span>
+      </div>
+      {svg}
+    </button>
+  );
+};
+
+export default CheckboxRow;
