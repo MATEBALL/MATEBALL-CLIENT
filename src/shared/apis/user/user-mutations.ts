23f00417f3ab3056e8c8252ced05981ee59c93e4
heesunee <heesun729@uos.ac.kr>
2025-08-29 14:56:55 +0900

    feat: 사용자 정보 설정 api 수정 (#323)

--- src/shared/apis/user/user-mutations.ts
+++ src/shared/apis/user/user-mutations.ts
@@ -4,23 +4,14 @@ import { USER_KEY } from '@constants/query-key';
 import queryClient from '@libs/query-client';
 import { mutationOptions } from '@tanstack/react-query';
 import type { responseTypes } from '@/shared/types/base-types';
-import type {
-  postAgreementInfoRequest,
-  postUserInfoNicknameRequest,
-  postUserInfoRequest,
-} from '@/shared/types/user-types';
+import type { postAgreementInfoRequest, postUserInfoRequest } from '@/shared/types/user-types';
 
 export const userMutations = {
-  NICKNAME: () =>
-    mutationOptions<responseTypes, Error, postUserInfoNicknameRequest>({
-      mutationKey: USER_KEY.NICKNAME(),
-      mutationFn: ({ nickname }) => post(END_POINT.POST_INFO_NICKNAME, { nickname }),
-    }),
-
   USER_INFO: () =>
     mutationOptions<responseTypes, Error, postUserInfoRequest>({
       mutationKey: USER_KEY.INFO(),
-      mutationFn: ({ gender, birthYear }) => post(END_POINT.USER_INFO, { gender, birthYear }),
+      mutationFn: ({ nickname, introduction, birthYear, gender }) =>
+        post(END_POINT.USER_INFO, { nickname, introduction, birthYear, gender }),
     }),
 
   LOGOUT: () =>
