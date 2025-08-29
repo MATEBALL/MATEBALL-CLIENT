c6a4447b7d7c3dea8348d5fa0a3ff8bb7599af7e
heesunee <heesun729@uos.ac.kr>
2025-08-29 13:58:31 +0900

    feat: LOGOUT시 쿼리 삭제, 취소 (#305)

--- src/shared/apis/user/user-mutations.ts
+++ src/shared/apis/user/user-mutations.ts
@@ -1,6 +1,7 @@
 import { post } from '@apis/base/http';
 import { END_POINT } from '@constants/api';
 import { USER_KEY } from '@constants/query-key';
+import queryClient from '@libs/query-client';
 import { mutationOptions } from '@tanstack/react-query';
 import type { responseTypes } from '@/shared/types/base-types';
 import type { postUserInfoNicknameRequest, postUserInfoRequest } from '@/shared/types/user-types';
@@ -22,8 +23,10 @@ export const userMutations = {
     mutationOptions<responseTypes, Error, void>({
       mutationKey: USER_KEY.LOGOUT(),
       mutationFn: () => post(END_POINT.POST_AUTH_LOGOUT),
-      onSuccess: () => {
-        window.location.reload();
+      onSuccess: async () => {
+        await queryClient.cancelQueries({ queryKey: USER_KEY.ALL });
+
+        queryClient.removeQueries({ queryKey: USER_KEY.ALL });
       },
     }),
 };
