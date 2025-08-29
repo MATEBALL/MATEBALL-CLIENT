b01681ae46dd05d9474cc0d8d710fba05acd9083
heesunee <heesun729@uos.ac.kr>
2025-08-29 14:41:56 +0900

    feat: 약관 동의 노션 페이지 연결 (#323)

---
+++ src/pages/sign-up/constants/LINK.ts
@@ -0,0 +1,3 @@
+export const TERMS_LINK = 'https://www.notion.so/22bebc583aa58015b71dd75cf7884d33?pvs=4';
+
+export const PRIVACY_LINK = 'https://www.notion.so/22bebc583aa580ce8c31eb0fbe84ac58?pvs=4';
