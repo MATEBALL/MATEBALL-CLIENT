ffbbe6975b2e4facb7137cac6f6d3e6770389c05
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-09-04 23:54:26 +0900

    feat: SP1 닉네임 중복확인 api 연결 (#336)
    * feat: 중복 확인 버튼 추가 (#327)
    
    * feat: 중복 확인 버튼 추가 (#327)
    
    * feat: 닉네임 중복 확인 api 추가 (#327)
    
    * feat: 중복확인 api 연결 (#327)
    
    * feat: 프로필 수정 내 중복확인 api 추가 (#327)
    
    * feat: 프로필 수정 페이지 내 중복확인 api 연결 (#327)
    
    ---------
    
    Co-authored-by: heesunee <heesun729@uos.ac.kr>

--- src/shared/apis/user/user-queries.ts
+++ src/shared/apis/user/user-queries.ts
@@ -1,7 +1,9 @@
 import { get } from '@apis/base/http';
 import { END_POINT } from '@constants/api';
 import { USER_KEY } from '@constants/query-key';
+import { HTTP_STATUS } from '@constants/response';
 import { queryOptions } from '@tanstack/react-query';
+import axios from 'axios';
 import type { getMatchConditionResponse, getUserInfoResponse } from '@/shared/types/user-types';
 
 export const userQueries = {
@@ -23,5 +25,27 @@ export const userQueries = {
     queryOptions<getMatchConditionResponse>({
       queryKey: USER_KEY.MATCH_CONDITION(),
       queryFn: () => get<getMatchConditionResponse>(END_POINT.MATCH_CONDITION),
+    }),
+
+  NICKNAME_CHECK: (nickname: string) =>
+    queryOptions<boolean>({
+      queryKey: USER_KEY.NICKNAME_CHECK(nickname),
+      enabled: false,
+      queryFn: async () => {
+        try {
+          await get<void>(END_POINT.GET_NICKNAME_CHECK(nickname));
+          return true;
+        } catch (e) {
+          if (axios.isAxiosError(e) && e.response?.status === HTTP_STATUS.CONFLICT) {
+            return false;
+          }
+          throw e;
+        }
+      },
+      retry: (failureCount, error) => {
+        if (axios.isAxiosError(error) && error.response?.status === HTTP_STATUS.CONFLICT)
+          return false;
+        return failureCount < 2;
+      },
     }),
 };
