b42cfe9dd6530f8f70950a2c323f805f430b848b
bongtta <kce0419@naver.com>
2025-09-05 10:31:15 +0900

    fix: 라벨 수정 (#337)

--- src/pages/edit-profile/constants/edit-profile.ts
+++ src/pages/edit-profile/constants/edit-profile.ts
@@ -3,14 +3,14 @@ export const PROFILE_SYNC_MATE = ['같은 팀 메이트', '상관없어요'];
 export const PROFILE_VIEWING_STYLE = [
   {
     id: 1,
-    label: '열정 응원러',
+    label: '열정응원러',
   },
   {
     id: 2,
-    label: '경기 집중러',
+    label: '경기집중러',
   },
   {
     id: 3,
-    label: '직관 먹방러',
+    label: '직관먹방러',
   },
 ];
