c785b61e761977cca205a0a1847be9832dd8f822
bongtta <kce0419@naver.com>
2025-09-06 21:00:09 +0900

    fix: 텍스트 중앙 정렬 (#352)

--- src/shared/components/card/banner-card/banner-card.tsx
+++ src/shared/components/card/banner-card/banner-card.tsx
@@ -11,7 +11,7 @@ const BannerCard = ({ text, subText, className }: CardBannerProps) => {
   return (
     <div className={cn('relative w-full', className)}>
       <img src={BannerImg} alt="홈배너" className="w-full" />
-      <div className="absolute inset-0 flex-col gap-[0.4rem] py-[2.2rem] pl-[2.4rem]">
+      <div className="absolute inset-0 flex-col justify-center gap-[0.4rem] py-[2.2rem] pl-[2.4rem]">
         <p className="cap_14_m text-gray-800">{subText}</p>
         <p className="subhead_18_sb text-gray-black">{text}</p>
       </div>
