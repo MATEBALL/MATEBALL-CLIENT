cf1e359bf171583f37d7247d307f1df536c0c049
bongtta <kce0419@naver.com>
2025-09-08 03:04:16 +0900

    fix: 제네릭 순서 수정 및 반환 타입 명시 (#378)

--- src/shared/apis/alarm/alarm-mutations.ts
+++ src/shared/apis/alarm/alarm-mutations.ts
@@ -6,9 +6,12 @@ import { mutationOptions } from '@tanstack/react-query';
 
 export const alarmMutations = {
   READ_ALL_ALARMS: () =>
-    mutationOptions<Error, void>({
+    mutationOptions<void, Error, void>({
       mutationKey: ALARM_KEY.READ_ALL(),
-      mutationFn: () => post(END_POINT.POST_READ_ALL_ALARMS),
+      mutationFn: () => post<void>(END_POINT.POST_READ_ALL_ALARMS),
+      onMutate: async () => {
+        queryClient.setQueryData(ALARM_KEY.HAS_UNREAD, false);
+      },
       onSuccess: () => {
         queryClient.invalidateQueries({ queryKey: ALARM_KEY.HAS_UNREAD });
       },
