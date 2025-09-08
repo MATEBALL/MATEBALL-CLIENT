3475d8f315f19250a9d9753cdd8f3d1990a4ea41
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-09-08 16:53:18 +0900

    fix: hotfix design (#388)
    Co-authored-by: heesunee <heesun729@uos.ac.kr>

--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -8,7 +8,7 @@ const SignUp = () => {
   const { Funnel, Step, goNext } = useFunnel(SIGNUP_STEPS, ROUTES.HOME);
 
   return (
-    <div className="flex-col bg-gray-white">
+    <div className="h-full flex-col bg-gray-white">
       <Funnel>
         <Step name="AGREEMENT">
           <AgreementStep next={goNext} />
