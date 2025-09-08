01433b2c7e588fa5900d43bebb3daebbe3aebf18
yeeeww <yeangsshi@ewhain.net>
2025-09-08 14:45:29 +0900

    fix: 홈으로 페이지 이동할 때 배너 이미지 로드 전에 텍스트만 뜨는 문제 해결 (#384)

--- src/shared/components/card/banner-card/banner-card.tsx
+++ src/shared/components/card/banner-card/banner-card.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@libs/cn';
+import { useState } from 'react';
 import BannerImg from '/images/card_banner_img.webp';
 
 interface CardBannerProps {
@@ -9,13 +10,17 @@ interface CardBannerProps {
 }
 
 const BannerCard = ({ text, subText, onClick }: CardBannerProps) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <button type="button" className={cn('relative cursor-pointer text-left')} onClick={onClick}>
-      <img src={BannerImg} alt="홈배너" className="w-full" />
-      <div className="absolute inset-0 flex-col justify-center gap-[0.4rem] py-[2.2rem] pl-[2.4rem]">
-        <p className="cap_14_m text-gray-800">{subText}</p>
-        <p className="subhead_18_sb text-gray-black">{text}</p>
-      </div>
+      <img src={BannerImg} alt="홈배너" className="w-full" onLoad={() => setImageLoaded(true)} />
+      {imageLoaded && (
+        <div className="absolute inset-0 flex flex-col justify-center gap-[0.4rem] py-[2.2rem] pl-[2.4rem]">
+          <p className="cap_14_m text-gray-800">{subText}</p>
+          <p className="subhead_18_sb text-gray-black">{text}</p>
+        </div>
+      )}
     </button>
   );
 };
