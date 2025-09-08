f2347b0e39ef87a10c215318a463066f257832db
heesunee <heesun729@uos.ac.kr>
2025-09-08 16:14:19 +0900

    fix: design 최종 반영 (#388)

--- src/pages/sign-up/components/signup-step.tsx
+++ src/pages/sign-up/components/signup-step.tsx
@@ -118,9 +118,9 @@ const SignupStep = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="h-svh w-full flex-col justify-between gap-[4rem] px-[1.6rem] pt-[4rem] pb-[1.6rem]"
+      className="h-full w-full flex-col justify-between gap-[4rem] px-[1.6rem] pt-[4rem] pb-[1.6rem]"
     >
-      <div className="h-full w-full flex-col gap-[4rem]">
+      <div className="w-full flex-1 flex-col gap-[4rem]">
         <h1 className="title_24_sb whitespace-pre-line">{NICKNAME_TITLE}</h1>
         <div className=" flex-col gap-[2.4rem]">
           <div className="flex-col gap-[0.8rem]">
