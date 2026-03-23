70d8c41052f63c6ba2a7a0bab9f1cf004599eb54
bongtta <kce0419@naver.com>
2026-03-24 00:29:45 +0900

    feat: 마이페이지 프로필 카드 제작 (#399)

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,5 +1,6 @@
 import { userMutations } from '@apis/user/user-mutations';
 import { userQueries } from '@apis/user/user-queries';
+import Button from '@components/button/button/button';
 import Divider from '@components/divider/divider';
 import Footer from '@components/footer/footer';
 import { FEEDBACK_LINK, REQUEST_LINK } from '@pages/profile/constants/link';
@@ -7,6 +8,10 @@ import { ROUTES } from '@routes/routes-config';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import ProfileCard from './components/profile-card';
+
+// TODO: api 연결
+const MOCK_MATCH_COUNT = 1;
+const MOCK_AVERAGE_SEASON_COUNT = 3;
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -18,15 +23,20 @@ const Profile = () => {
 
   return (
     <div className="h-full flex-col-between">
-      <div className="w-full flex-col-center gap-[3.2rem] px-[1.6rem] pt-[1.6rem] pb-[5.6rem]">
+      <div className="w-full flex-col-center gap-[1.6rem] px-[1.6rem] pt-[1.6rem] pb-[5.6rem]">
         <ProfileCard
           nickname={data.nickname ?? ''}
+          imgUrl={data.imgUrl ?? ''}
           team={data.team ?? ''}
           style={data.style ?? ''}
-          imgUrl={data.imgUrl ?? ''}
-          matchCnt={data.matchCnt ?? 0}
-          avgSeason={data.avgSeason ?? 0}
+          matchCnt={MOCK_MATCH_COUNT}
+          avgSeason={MOCK_AVERAGE_SEASON_COUNT}
           onEditProfile={() => navigate(ROUTES.PROFILE_EDIT)}
+        />
+        <Button
+          size="L"
+          label="프로필 · 매칭 조건 수정"
+          onClick={() => navigate(ROUTES.PROFILE_EDIT)}
         />
         <section className="w-full flex-col items-start">
           <a
