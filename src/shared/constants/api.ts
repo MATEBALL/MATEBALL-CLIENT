96e68a94847f6fb4d8b7e500d75451e7d0dbcf54
bongtta <kce0419@naver.com>
2026-03-24 01:25:33 +0900

    fix: end point 수정 (#399)

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -17,7 +17,7 @@ export const END_POINT = {
   GET_USER_INFO: '/v1/users/info',
   POST_INFO_NICKNAME: '/v1/users/info/nickname',
   POST_EDIT_PROFILE: '/v2/users/info',
-  MATCH_CONDITION: '/v2/users/match-condition',
+  MATCH_CONDITION: '/v3/users/match-condition',
 
   // 경기 관련
   GET_GAME_SCHEDULE: (date: string) => `/v1/users/game/schedule?date=${date}`,
