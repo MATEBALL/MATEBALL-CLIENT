ddbeaa92146f6d255b8b93756bb2f24a7846738d
bongtta <kce0419@naver.com>
2026-03-24 13:14:49 +0900

    style: text, color 수정 (#409)

--- src/pages/home/components/calendar-button.tsx
+++ src/pages/home/components/calendar-button.tsx
@@ -11,8 +11,8 @@ const CalendarButton = ({ onOpenBottomSheet }: CalendarButtonProps) => {
       className="h-[3.2rem] flex-row-center cursor-pointer gap-[0.8rem] rounded-[8px]p-[0.8rem]"
       onClick={onOpenBottomSheet}
     >
-      <Icon name="calendar" size={2.4} className="text-gray-white" />
-      <h1 className="cap_14_sb text-gray-white">날짜 선택</h1>
+      <Icon name="calendar" size={2.4} className="text-gray-200" />
+      <h1 className="cap_14_sb text-gray-200">전체보기</h1>
     </button>
   );
 };
