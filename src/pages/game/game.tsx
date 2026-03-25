80ada7ee84f971912971861d2531d1f8b5660a11
bongtta <kce0419@naver.com>
2026-03-25 14:55:13 +0900

    feat: 경기 매칭 페이지 헤더 구현 및 api 연결 (#412)

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -1,8 +1,42 @@
+import { gameQueries } from '@apis/game/game-queries';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useOutletContext, useParams } from 'react-router-dom';
 
-const Game = () => {
-  return (
-    <div>Game</div>
-  )
+interface LayoutOutletContext {
+  setHeaderTitle: (title: string) => void;
 }
 
-export default Game
+const Game = () => {
+  const { date, gameId } = useParams();
+  const { setHeaderTitle } = useOutletContext<LayoutOutletContext>();
+
+  const { data: gameSchedule } = useQuery({
+    ...gameQueries.GAME_LIST(date ?? ''),
+    enabled: !!date,
+  });
+
+  useEffect(() => {
+    const currentGame = gameSchedule?.find((game) => String(game.id) === gameId);
+
+    if (!currentGame) {
+      setHeaderTitle('');
+      return;
+    }
+
+    setHeaderTitle(`${currentGame.awayTeam} vs ${currentGame.homeTeam}`);
+
+    return () => {
+      setHeaderTitle('');
+      return;
+    };
+  }, [gameId, gameSchedule, setHeaderTitle]);
+
+  return (
+    <div>
+      <div>Game</div>
+    </div>
+  );
+};
+
+export default Game;
