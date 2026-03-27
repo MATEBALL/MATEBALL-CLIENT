b284187657d859d9cb10e89ffc7eb174d1a4d77f
bongtta <kce0419@naver.com>
2026-03-27 20:26:38 +0900

    fix: 리뷰 반영 (#399)

--- src/pages/profile/components/profile-card.tsx
+++ src/pages/profile/components/profile-card.tsx
@@ -1,5 +1,5 @@
 import Button from '@components/button/button/button';
-import type { ChipColor } from '@components/card/match-card/types/card';
+import { getChipColor } from '@components/card/match-card/utils/get-chip-color';
 import Chip from '@components/chip/chip';
 import Divider from '@components/divider/divider';
 import { ROUTES } from '@routes/routes-config';
@@ -12,11 +12,13 @@ interface ProfileCardProps {
   style: string;
   matchCnt?: number;
   avgSeason?: number;
-  onEditProfile?: () => void;
 }
 
 const ProfileCard = ({ nickname, imgUrl, team, style, matchCnt, avgSeason }: ProfileCardProps) => {
   const navigate = useNavigate();
+
+  const teamChipColor = getChipColor(team);
+  const styleChipColor = getChipColor(style);
 
   return (
     <div className="w-full flex-col gap-[1.2rem] rounded-[12px] bg-gray-950 p-[2rem]">
@@ -30,8 +32,8 @@ const ProfileCard = ({ nickname, imgUrl, team, style, matchCnt, avgSeason }: Pro
           <div className="flex-col gap-[0.4rem]">
             <p className="subhead_18_sb text-gray-white">{nickname}</p>
             <div className="flex gap-[0.4rem]">
-              <Chip label={team} bgColor={team as ChipColor} textColor={team as ChipColor} />
-              <Chip label={style} bgColor={style as ChipColor} textColor={style as ChipColor} />
+              <Chip label={team} bgColor={teamChipColor} textColor={teamChipColor} />
+              <Chip label={style} bgColor={styleChipColor} textColor={styleChipColor} />
             </div>
           </div>
         </div>
@@ -48,14 +50,14 @@ const ProfileCard = ({ nickname, imgUrl, team, style, matchCnt, avgSeason }: Pro
       <div className="w-full flex-row-evenly rounded-[8px] bg-gray-700 px-[1.2rem] py-[0.8rem]">
         <div className="flex-col-between gap-[0.2rem]">
           <p className="cap_14_sb text-gray-400">함께한 매칭</p>
-          <p className="head_20_sb text-gray-white">{matchCnt}</p>
+          <p className="head_20_sb text-gray-white">{matchCnt ?? '-'}</p>
         </div>
         <div className="h-[3.3rem]">
           <Divider direction="vertical" thickness={0.1} color="bg-gray-600" />
         </div>
         <div className="flex-col-between gap-[0.2rem]">
           <p className="cap_14_sb text-gray-400">시즌 평균 직관</p>
-          <p className="head_20_sb text-gray-white">{avgSeason}</p>
+          <p className="head_20_sb text-gray-white">{avgSeason ?? '-'}</p>
         </div>
       </div>
     </div>
