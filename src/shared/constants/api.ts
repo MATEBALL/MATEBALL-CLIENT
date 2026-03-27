e4965f78f9793b225cd48e69ccc336d13dcb93df
bongtta <kce0419@naver.com>
2026-03-28 05:39:15 +0900

    feat: api 추가 (#414)

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -38,6 +38,7 @@ export const END_POINT = {
   GET_GROUP_STATUS: (status: string) => `/v2/users/match-stage/group?status=${status}`,
 
   GET_MATCH_DETAIL: (matchId: number | string) => `/v1/users/match/${matchId}`,
+  GET_MATCH_MEMBERS: (matchId: number | string) => `/v3/users/match/members/${matchId}`,
   POST_MATCH_REQUEST: (matchId: number | string) => `/v1/users/match-request/${matchId}`,
   PATCH_MATCH_ACCEPT: (matchId: number | string) => `/v1/users/match-accept/${matchId}`,
   PATCH_MATCH_REJECT: (matchId: number | string) => `/v1/users/match-reject/${matchId}`,
