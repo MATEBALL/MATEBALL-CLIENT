65c2b7234db89aa5393d3f50711c6ad3b9a89e1c
bongtta <kce0419@naver.com>
2026-04-07 15:41:47 +0900

    feat: 가이드 메시지 추가 (#419)

--- src/pages/match/constants/matching.ts
+++ src/pages/match/constants/matching.ts
@@ -46,6 +46,9 @@ export const MATCHING_GUIDE_MESSAGE_DESCRIPTION =
 export const GROUP_MATCHING_CREATED_DESCRIPTION =
   '매칭 현황에서 요청을 확인하고\n채팅방에서 메이트와 만날 수 있어요.';
 
+export const MATCHING_GUIDE_CREATED_DESCRIPTION =
+  '매칭현황에서 요청을 확인하고\n채팅방에서 메이트와 만날 수 있어요.';
+
 export const CLICKABLE_STATUS_MAP: Record<string, string> = {
   '매칭 완료': 'success',
   '승인 완료': 'agree',
