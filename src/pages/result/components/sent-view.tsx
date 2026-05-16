587de45444f77f66029cbe85b1b1c8f7c1162837
bongtta <kce0419@naver.com>
2026-05-16 17:10:31 +0900

    style: 요청 전송 완료 페이지 상단 여백 추가 (#428)

--- src/pages/result/components/sent-view.tsx
+++ src/pages/result/components/sent-view.tsx
@@ -13,7 +13,7 @@ const SentView = () => {
   const handleGoMatch = () => navigate(ROUTES.MATCH);
 
   return (
-    <div className="h-full flex-col-between">
+    <div className="h-full flex-col-between pt-[9.6rem]">
       <section className="flex-col-center gap-[4rem] pt-[4rem]">
         <h2 className="title_24_sb">매칭 요청이 전송되었어요!</h2>
         <div className="h-[16rem] w-[16rem] flex-row-center">
