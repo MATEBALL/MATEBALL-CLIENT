5c7d4a4ebaee42ab36c9136d7f7d4e0478142fb6
CHAEEUN KIM <154000318+bongtta@users.noreply.github.com>
2026-07-16 05:42:53 +0900

    feat: SP2 history state에 날짜 정보 저장 (#439)
    * fix: 로고 클릭시 홈 새로고침 (#438)
    
    * style: 게임 카드 버튼 스타일 수정 (#438)
    
    * feat: 날짜 정보 history state에 저장 (#438)

--- src/shared/components/header/utils/get-header.tsx
+++ src/shared/components/header/utils/get-header.tsx
@@ -10,7 +10,7 @@ export const getHeaderContent = (
   headerTitle: string,
 ) => {
   const handleLogoClick = () => {
-    if (navigate) navigate({ pathname: ROUTES.HOME, search: '' });
+    window.location.href = ROUTES.HOME;
   };
 
   const handleBackClick = () => {
